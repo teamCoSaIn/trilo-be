563385a66bd4609e3aac97b4b7ee5f300353042e
ttasjwi <88282356+ttasjwi@users.noreply.github.com>
2023-02-24 00:42:03 +0900

    [TRL-108] infra: 무중단 배포 구축(GitHub Actions + CodeDeploy + Nginx) (#5)
    * [TRL-108] infra: gradle 빌드 프로세스 작성
    
    * [TRL-108] infra: 압축 후 S3 업로드 자동화
    
    * [TRL-108] infra: CodeDeploy 전달 프로세스 추가
    
    * [TRL-108] infra: 배포상태 확인용 API 및 설정 파일 추가
    
    * [TRL-108] infra: 블루/그린 배포 프로세스 추가

---
+++ src/main/java/com/cosain/trilo/deploy/DeployController.java
@@ -0,0 +1,22 @@
+package com.cosain.trilo.deploy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeployController {
+
+    @Value("${deploy-module.version}")
+    private String version;
+
+    @GetMapping("/deploy/version")
+    private String version() {
+        return String.format("Project.version : %s", version);
+    }
+
+    @GetMapping("/deploy/health")
+    public String checkHealth() {
+        return "healthy";
+    }
+}
