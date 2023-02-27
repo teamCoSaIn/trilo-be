4ccfd60ead09ee7ebf5b31aac7b3cee2fd2402a4
pia2011 <53935439+pia2011@users.noreply.github.com>
2023-02-27 23:09:30 +0900

    [TRL-111] chore: Spring Rest Docs 환경 설정 (#7)
    * [TRL-111] chore: REST Docs 의존 라이브러리 추가 및 설정
    
    * [TRL-111] chore: REST Docs 테스트를 위한 코드 추가
    
    * [TRL-111] chore: HTML 렌더링을 위한 asciidoc 파일 추가

---
+++ src/test/java/com/cosain/trilo/support/RestDocsTestSupport.java
@@ -0,0 +1,41 @@
+package com.cosain.trilo.support;
+
+import com.cosain.trilo.config.RestDocsConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+@ExtendWith(RestDocumentationExtension.class)
+@Import(RestDocsConfig.class)
+@AutoConfigureRestDocs
+public class RestDocsTestSupport {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected RestDocumentationResultHandler restDocs;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
+                .alwaysDo(MockMvcResultHandlers.print())
+                .alwaysDo(restDocs)
+                .build();
+    }
+
+
+
+}
