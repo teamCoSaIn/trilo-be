7414b9fe55cb8f93d00cf7d4a05f9f36afb57f10
ttasjwi <ttasjwi920@gmail.com>
2023-03-06 17:08:37 +0900

    [TRL-131] infra: 배포 java 실행 옵션에 설정 분리 반영

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -20,6 +20,6 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/trilo/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} -Dspring.profiles.active=prod -Dspring.config.location=/home/ec2-user/application-prod.yml /home/ec2-user/trilo/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
