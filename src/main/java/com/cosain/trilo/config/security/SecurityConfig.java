73d9325d386bb36c0383a2fb10f1e190e8013982
pia2011 <kks01650165@gmail.com>
2023-03-16 11:08:38 +0900

    [TRL-143] chore: SpringSecurity 의존성 추가
    Spring Security 기본 설정으로 인해 모든 요청에 인증이 필요하므로 이전에 정의해둔 deploy 관련 API 접근만 허용하는 설정 추가해서 등록

---
+++ src/main/java/com/cosain/trilo/config/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.cosain.trilo.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(HttpMethod.GET, "/deploy/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+
+}
