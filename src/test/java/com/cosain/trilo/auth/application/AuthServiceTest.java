85561b0a2863262bf3597ed73dd70e0a31eb2f7a
pia2011 <kks01650165@gmail.com>
2023-03-23 15:42:41 +0900

    [TRL-119] test: 접근 토큰 재발급 응용 서비스 테스트

---
+++ src/test/java/com/cosain/trilo/auth/application/AuthServiceTest.java
@@ -0,0 +1,54 @@
+package com.cosain.trilo.auth.application;
+
+import com.cosain.trilo.auth.domain.TokenRepository;
+import com.cosain.trilo.auth.infra.TokenAnalyzer;
+import com.cosain.trilo.auth.infra.TokenProvider;
+import com.cosain.trilo.common.exception.NotExistRefreshTokenException;
+import com.cosain.trilo.common.exception.NotValidTokenException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class AuthServiceTest {
+    @InjectMocks
+    private AuthService authService;
+    @Mock
+    private TokenRepository tokenRepository;
+    @Mock
+    private TokenAnalyzer tokenAnalyzer;
+    @Mock
+    private TokenProvider tokenProvider;
+
+
+    @Test
+    void ACCESS_토큰_재발급(){
+        given(tokenAnalyzer.validateToken(any())).willReturn(true);
+        given(tokenProvider.createAccessToken(any())).willReturn("accessToken");
+        given(tokenRepository.existsById(any())).willReturn(true);
+
+        String accessToken = authService.reissueAccessToken(any());
+        System.out.println(accessToken);
+    }
+
+    @Test
+    void 접근토큰_재발급시_재발급_토큰이_유효하지_않다면_에러를_발생시킨다(){
+        given(tokenAnalyzer.validateToken(any())).willReturn(false);
+
+        Assertions.assertThatThrownBy(() -> authService.reissueAccessToken(any())).isInstanceOf(NotValidTokenException.class);
+    }
+
+    @Test
+    void 접근토큰_재발급시_REDIS에_재발급_토큰이_존재하지_않는다면_에러를_발생시킨다(){
+        given(tokenAnalyzer.validateToken(any())).willReturn(true);
+        given(tokenRepository.existsById(any())).willReturn(false);
+
+        Assertions.assertThatThrownBy(() -> authService.reissueAccessToken(any())).isInstanceOf(NotExistRefreshTokenException.class);
+    }
+}
