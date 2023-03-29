997a6646f4b9bb88e7c1228498650dd4eebc584d
ttasjwi <ttasjwi920@gmail.com>
2023-03-29 11:30:53 +0900

    [TRL-167] feat: 쿼리파라미터에 lang이 올 때, 해당 값을 언어/국가 정보로 인식하도록 함
    - LocaleChangeInterceptor를 통해 "lang" 파라미터가 전달될 시, 해당 파라미터 값을 기반으로 언어/국가 정보를 인식하도록 한다.

--- src/main/java/com/cosain/trilo/config/message/LocaleConfiguration.java
+++ src/main/java/com/cosain/trilo/config/message/LocaleConfiguration.java
@@ -3,8 +3,10 @@ package com.cosain.trilo.config.message;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import java.util.Locale;
 
@@ -20,6 +22,21 @@ public class LocaleConfiguration implements WebMvcConfigurer {
         var resolver = new AcceptHeaderLocaleResolver();
         resolver.setDefaultLocale(Locale.KOREAN); // 언어 & 국가 정보가 없는 경우 한국으로 인식하도록 설정
         return resolver;
+    }
+
+    /**
+     * 요청을 가로채서, 쿼리 파라미터에 lang 파라미터가 전달됐을 경우 해당 값을 클라이언트의 언어 & 국가 정보로 인식하도록 한다.
+     */
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 
 }
