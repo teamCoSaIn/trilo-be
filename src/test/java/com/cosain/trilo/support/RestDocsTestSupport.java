35a21a2fcffd078bdf34946751f166e4599cd8b3
pia2011 <kks01650165@gmail.com>
2023-04-01 02:00:58 +0900

    [TRL-171] test: API 문서화 테스트 기본 설정 클래스 수정

--- src/test/java/com/cosain/trilo/support/RestDocsTestSupport.java
+++ src/test/java/com/cosain/trilo/support/RestDocsTestSupport.java
@@ -4,40 +4,30 @@ import com.cosain.trilo.config.RestDocsConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.context.annotation.Import;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @ExtendWith(RestDocumentationExtension.class)
 @Import(RestDocsConfig.class)
-@AutoConfigureRestDocs
-public class RestDocsTestSupport {
-
-    @Autowired
-    protected MockMvc mockMvc;
+public class RestDocsTestSupport extends RestControllerTest{
 
     @Autowired
     protected RestDocumentationResultHandler restDocs;
 
     @BeforeEach
-    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+    void setUp(RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
                 .apply(documentationConfiguration(restDocumentation))
                 .apply(springSecurity())
                 .alwaysDo(MockMvcResultHandlers.print())
                 .alwaysDo(restDocs)
                 .build();
     }
-
-
-
 }
