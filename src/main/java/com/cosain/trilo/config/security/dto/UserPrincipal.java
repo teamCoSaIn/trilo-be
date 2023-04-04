c958c601ec894723463ec35508cbcbae5e986edb
pia2011 <53935439+pia2011@users.noreply.github.com>
2023-04-04 15:11:16 +0900

    [TRL-165] feat: @AuthenticationPrincipal 커스터마이징 (#32)
    - LoginUser 어노테이션은 컨트롤러에서 UserPrincipal 객체를 파라미터로 받아와서 사용할 수 있도록 해줍니다.
    - 이를 통해 컨트롤러 코드의 가독성을 높이고, 보안적인 이슈도 예방할 수 있습니다.

--- src/main/java/com/cosain/trilo/config/security/dto/UserPrincipal.java
+++ src/main/java/com/cosain/trilo/config/security/dto/UserPrincipal.java
@@ -1,13 +1,14 @@
 package com.cosain.trilo.config.security.dto;
 
 import com.cosain.trilo.user.domain.User;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
-
+@Getter
 public class UserPrincipal implements OAuth2User {
 
     private User user;
