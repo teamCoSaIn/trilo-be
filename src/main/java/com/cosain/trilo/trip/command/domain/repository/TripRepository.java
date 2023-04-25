a0cb8dfe8c8705aff184a7df4c5367d0d9ca9605
pia2011 <kks01650165@gmail.com>
2023-04-25 16:09:23 +0900

    [TRL-123] feat: TripRepository 에 findByIdWithDays() 추가
    - 패치 전략이 지연 로딩으로 되어 있기 때문에 즉시 로딩을 통해 Trip과 연관 관계에 놓여있는 Day들을 함께 가져옴

--- src/main/java/com/cosain/trilo/trip/command/domain/repository/TripRepository.java
+++ src/main/java/com/cosain/trilo/trip/command/domain/repository/TripRepository.java
@@ -2,6 +2,15 @@ package com.cosain.trilo.trip.command.domain.repository;
 
 import com.cosain.trilo.trip.command.domain.entity.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface TripRepository extends JpaRepository<Trip, Long> {
+
+    @Query("SELECT t" +
+            " FROM Trip as t JOIN FETCH t.days" +
+            " WHERE t.id = :id")
+    Optional<Trip> findByIdWithDays(@Param("id") Long tripId);
 }
