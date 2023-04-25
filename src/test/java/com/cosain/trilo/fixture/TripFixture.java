b44ecbe54448a9d36f9752e9f08efa97cb503e56
pia2011 <kks01650165@gmail.com>
2023-04-25 16:24:45 +0900

    [TRL-123] test: 테스트 편의성을 위해 TripFixture 정의

---
+++ src/test/java/com/cosain/trilo/fixture/TripFixture.java
@@ -0,0 +1,65 @@
+package com.cosain.trilo.fixture;
+
+import com.cosain.trilo.trip.command.domain.entity.Day;
+import com.cosain.trilo.trip.command.domain.entity.Trip;
+import com.cosain.trilo.trip.command.domain.vo.TripPeriod;
+import com.cosain.trilo.trip.command.domain.vo.TripStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.time.LocalDate;
+
+public enum TripFixture {
+
+    UNDECIDED_TRIP(TripStatus.UNDECIDED),
+    DECIDED_TRIP(TripStatus.DECIDED),
+    ;
+
+    private final TripStatus status;
+    TripFixture(TripStatus status){
+        this.status = status;
+    }
+
+    public Trip create(Long id, Long tripperId, String title){
+        return Trip.builder()
+                .id(id)
+                .tripperId(tripperId)
+                .title(title)
+                .status(this.status)
+                .build();
+    }
+
+    public Trip create(Long id, Long tripperId, String title, LocalDate startDate, LocalDate endDate){
+        List<Day> days = createDays(startDate, endDate, this.create(id, tripperId, title));
+        return Trip.builder()
+                .id(id)
+                .tripperId(tripperId)
+                .title(title)
+                .tripPeriod(TripPeriod.of(startDate, endDate))
+                .days(days)
+                .status(this.status)
+                .build();
+    }
+
+    public Trip create(Long tripperId, String title, LocalDate startDate, LocalDate endDate){
+        return Trip.builder()
+                .tripperId(tripperId)
+                .title(title)
+                .tripPeriod(TripPeriod.of(startDate, endDate))
+                .build();
+    }
+
+    private List<Day> createDays(LocalDate startDate, LocalDate endDate, Trip trip){
+        List<Day> days = new ArrayList<>();
+        LocalDate currendDate = startDate;
+
+        while(!currendDate.isAfter(endDate)){
+            days.add(Day.of(currendDate, trip));
+            currendDate = currendDate.plusDays(1);
+        }
+
+        return days;
+    }
+
+}
