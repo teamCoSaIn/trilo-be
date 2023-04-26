dae6f923dd23f9a02f8145abbd6b7ca17807d59d
ttasjwi <ttasjwi920@gmail.com>
2023-04-26 17:23:33 +0900

    [TRL-206] refactor: 기간에 변경사항이 없을 경우 리포지토리를 호출하지 않도록 함

--- src/main/java/com/cosain/trilo/trip/command/application/service/TripUpdateService.java
+++ src/main/java/com/cosain/trilo/trip/command/application/service/TripUpdateService.java
@@ -4,12 +4,15 @@ import com.cosain.trilo.trip.command.application.command.TripUpdateCommand;
 import com.cosain.trilo.trip.command.application.exception.TripNotFoundException;
 import com.cosain.trilo.trip.command.application.usecase.TripUpdateUseCase;
 import com.cosain.trilo.trip.command.domain.dto.ChangeTripPeriodResult;
+import com.cosain.trilo.trip.command.domain.entity.Day;
 import com.cosain.trilo.trip.command.domain.entity.Trip;
 import com.cosain.trilo.trip.command.domain.repository.DayRepository;
 import com.cosain.trilo.trip.command.domain.repository.TripRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -22,17 +25,30 @@ public class TripUpdateService implements TripUpdateUseCase {
     @Transactional
     public void updateTrip(Long tripId, Long tripperId, TripUpdateCommand updateCommand) {
         Trip trip = findTrip(tripId);
-        trip.changeTitle(updateCommand.getTitle());
 
+        trip.changeTitle(updateCommand.getTitle());
         ChangeTripPeriodResult result = trip.updatePeriod(updateCommand.getStartDate(), updateCommand.getEndDate());
-        dayRepository.deleteDays(result.getDeletedDays());
-        dayRepository.saveAll(result.getCreatedDays());
+
+        deleteUnnecessaryDays(result.getDeletedDays());
+        saveCreatedDays(result.getCreatedDays());
     }
 
     private Trip findTrip(Long tripId) {
-        return tripRepository
-                .findById(tripId)
+        return tripRepository.findById(tripId)
                 .orElseThrow(() -> new TripNotFoundException("일치하는 식별자의 Trip을 찾을 수 없음"));
     }
 
+    private void deleteUnnecessaryDays(List<Day> deletedDays) {
+        if (deletedDays.isEmpty()) {
+            return;
+        }
+        dayRepository.deleteDays(deletedDays);
+    }
+
+    private void saveCreatedDays(List<Day> createdDays) {
+        if (createdDays.isEmpty()) {
+            return;
+        }
+        dayRepository.saveAll(createdDays);
+    }
 }
