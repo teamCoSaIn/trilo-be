05ba28f4fdf565c3fed8204458fd1281e8ce8a98
pia2011 <kks01650165@gmail.com>
2023-05-01 00:20:52 +0900

    [TRL-158] chore: Spring MVC 에서 DTO 객체 바인딩을 하기위한 기본 생성자 추가

--- src/main/java/com/cosain/trilo/trip/command/presentation/schedule/dto/ScheduleUpdateRequest.java
+++ src/main/java/com/cosain/trilo/trip/command/presentation/schedule/dto/ScheduleUpdateRequest.java
@@ -1,11 +1,14 @@
 package com.cosain.trilo.trip.command.presentation.schedule.dto;
 
 import com.cosain.trilo.trip.command.application.command.ScheduleUpdateCommand;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ScheduleUpdateRequest {
     private String title;
     private String content;
