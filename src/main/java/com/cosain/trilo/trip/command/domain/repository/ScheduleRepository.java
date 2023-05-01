97be811e12a54d3e5a73abcba9808bdf2dd0a331
pia2011 <kks01650165@gmail.com>
2023-05-01 11:18:19 +0900

    [TRL-158] feat: findByIdWithTrip 기능 구현 및 테스트
    - schedule 조회 시에 trip과 함께 조회
    - 불필요한 Trip의 조회 쿼리 발생 제어

--- src/main/java/com/cosain/trilo/trip/command/domain/repository/ScheduleRepository.java
+++ src/main/java/com/cosain/trilo/trip/command/domain/repository/ScheduleRepository.java
@@ -1,8 +1,17 @@
 package com.cosain.trilo.trip.command.domain.repository;
 
 import com.cosain.trilo.trip.command.domain.entity.Schedule;
+import com.cosain.trilo.trip.command.domain.entity.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
+    @Query("SELECT s" +
+            " FROM Schedule as s JOIN FETCH s.trip" +
+            " WHERE s.id = :id")
+    Optional<Schedule> findByIdWithTrip(@Param("id") Long scheduleId);
 }
