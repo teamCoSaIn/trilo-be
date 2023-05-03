36a4721b22c30760a6b6f302b1df612843e5c69e
ttasjwi <ttasjwi920@gmail.com>
2023-05-03 23:25:49 +0900

    [TRL-229] feat: ScheduleIndex에 다음번 인덱스를 발급하는 기능 추가

--- src/main/java/com/cosain/trilo/trip/command/domain/vo/ScheduleIndex.java
+++ src/main/java/com/cosain/trilo/trip/command/domain/vo/ScheduleIndex.java
@@ -12,9 +12,9 @@ import lombok.*;
 @Embeddable
 public class ScheduleIndex {
 
-    private static final long DEFAULT_SEQUENCE_GAP = 10_000_000;
-    private static final long MAX_INDEX = 5_000_000_000_000_000_000L;
-    private static final long MIN_INDEX = -5_000_000_000_000_000_000L;
+    public static final long DEFAULT_SEQUENCE_GAP = 10_000_000;
+    public static final long MAX_INDEX_VALUE = 5_000_000_000_000_000_000L;
+    public static final long MIN_INDEX_VALUE = -5_000_000_000_000_000_000L;
 
     @Column(name = "schedule_index")
     private long value;
@@ -22,7 +22,7 @@ public class ScheduleIndex {
     public static final ScheduleIndex ZERO_INDEX = ScheduleIndex.of(0);
 
     public static ScheduleIndex of(long value) {
-        if (value > MAX_INDEX || value < MIN_INDEX) {
+        if (value > MAX_INDEX_VALUE || value < MIN_INDEX_VALUE) {
             throw new ScheduleIndexRangeException("[처리 필요] 유효한 인덱스 범위를 벗어난 인덱스를 생성하려 함.");
         }
         return new ScheduleIndex(value);
@@ -30,6 +30,10 @@ public class ScheduleIndex {
 
     private ScheduleIndex(long value) {
         this.value = value;
+    }
+
+    public ScheduleIndex generateNextIndex() {
+        return ScheduleIndex.of(value + DEFAULT_SEQUENCE_GAP);
     }
 
 }
--- src/test/java/com/cosain/trilo/unit/trip/command/domain/vo/ScheduleIndexTest.java
+++ src/test/java/com/cosain/trilo/unit/trip/command/domain/vo/ScheduleIndexTest.java
@@ -4,9 +4,11 @@ import com.cosain.trilo.trip.command.domain.exception.ScheduleIndexRangeExceptio
 import com.cosain.trilo.trip.command.domain.vo.ScheduleIndex;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("[TripCommand] ScheduleIndexTest")
@@ -42,6 +44,31 @@ public class ScheduleIndexTest {
         public void when_create_with_too_small_index_then_it_throws_ScheduleIndexRangeException(long indexValue) {
             assertThatThrownBy(() -> ScheduleIndex.of(indexValue))
                     .isInstanceOf(ScheduleIndexRangeException.class);
+        }
+
+    }
+
+    @Nested
+    @DisplayName("generateNextIndex 테스트")
+    class generateNextIndexTest {
+
+        @Test
+        @DisplayName("다음에 오는 순서가 범위를 벗어나는 경우 예외 발생")
+        public void when_nextIndex_is_too_big_index_then_it_throws_ScheduleIndexRangeException() {
+            ScheduleIndex index = ScheduleIndex.of(ScheduleIndex.MAX_INDEX_VALUE);
+
+            assertThatThrownBy(index::generateNextIndex)
+                    .isInstanceOf(ScheduleIndexRangeException.class);
+        }
+
+
+        @Test
+        @DisplayName("최댓값을 넘지않는 인덱스의 다음 인덱스 생성 시 정상적으로 인덱스 생성")
+        public void successfully_generate_nextIndex() {
+            ScheduleIndex index = ScheduleIndex.ZERO_INDEX;
+
+            ScheduleIndex nextIndex = index.generateNextIndex();
+            assertThat(nextIndex.getValue()).isEqualTo(index.getValue() + ScheduleIndex.DEFAULT_SEQUENCE_GAP);
         }
 
     }
