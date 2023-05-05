d8d5eac4bbf20522af246bc300b133c0a889d816
pia2011 <kks01650165@gmail.com>
2023-05-05 14:12:21 +0900

    [TRL-234] test: 리포지토리 테스트를 위한 사용자 정의 어노테이션 @RepositoryTest 정의

---
+++ src/test/java/com/cosain/trilo/support/RepositoryTest.java
@@ -0,0 +1,22 @@
+package com.cosain.trilo.support;
+
+import com.cosain.trilo.config.QueryDslConfig;
+import com.cosain.trilo.trip.query.infra.repository.trip.TripQueryRepositoryImpl;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
+@DirtiesContext
+@Import({QueryDslConfig.class, TripQueryRepositoryImpl.class})
+public @interface RepositoryTest {
+}
