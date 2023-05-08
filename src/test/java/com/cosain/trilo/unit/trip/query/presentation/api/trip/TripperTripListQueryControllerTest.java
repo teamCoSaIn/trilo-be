8b0b503389b89a9a3300fdd46489b1cb446f8918
pia2011 <kks01650165@gmail.com>
2023-05-08 19:25:28 +0900

    [TRL-118] feat: 여행 목록 조회 컨트롤러 구현 및 테스트

--- src/test/java/com/cosain/trilo/unit/trip/query/presentation/api/trip/TripperTripListQueryControllerTest.java
+++ src/test/java/com/cosain/trilo/unit/trip/query/presentation/api/trip/TripperTripListQueryControllerTest.java
@@ -2,13 +2,26 @@ package com.cosain.trilo.unit.trip.query.presentation.api.trip;
 
 
 import com.cosain.trilo.support.RestControllerTest;
+import com.cosain.trilo.trip.query.application.usecase.TripListSearchUseCase;
 import com.cosain.trilo.trip.query.presentation.trip.TripperTripListQueryController;
+import com.cosain.trilo.trip.query.presentation.trip.dto.TripDetailResponse;
+import com.cosain.trilo.trip.query.presentation.trip.dto.TripPageResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
-import org.springframework.security.test.context.support.WithMockUser;
 
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -18,15 +31,29 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(TripperTripListQueryController.class)
 class TripperTripListQueryControllerTest extends RestControllerTest {
 
+    @MockBean
+    private TripListSearchUseCase tripListSearchUseCase;
+    private final String ACCESS_TOKEN = "Bearer accessToken";
+
+
     @Test
-    @DisplayName("인증된 사용자 요청 -> 미구현 500")
-    @WithMockUser
+    @DisplayName("인증된 사용자 요청 -> 회원 여행 목록 조회")
     public void findTripperTripList_with_authorizedUser() throws Exception {
-        mockMvc.perform(get("/api/trips?tripper-id=1"))
-                .andDo(print())
-                .andExpect(status().isInternalServerError())
-                .andExpect(jsonPath("$.errorCode").exists())
-                .andExpect(jsonPath("$.errorMessage").exists());
+
+        mockingForLoginUserAnnotation();
+        TripDetailResponse dto1 = TripDetailResponse.of(1L, "여행 제목 1", "DECIDED", LocalDate.of(2023, 5, 10), LocalDate.of(2023, 5, 15));
+        TripDetailResponse dto2 = TripDetailResponse.of(134L, "여행 제목 2", "DECIDED", LocalDate.of(2023, 5, 10), LocalDate.of(2023, 5, 15));
+        TripDetailResponse dto3 = TripDetailResponse.of(1423L, "여행 제목 3", "DECIDED", LocalDate.of(2023, 5, 10), LocalDate.of(2023, 5, 15));
+        TripPageResponse tripPageResponse = TripPageResponse.of(List.of(dto1, dto2, dto3), true);
+        given(tripListSearchUseCase.searchTripDetails(eq(1L), any(Pageable.class))).willReturn(tripPageResponse);
+
+        mockMvc.perform(get("/api/trips?tripper-id=1")
+                        .header(HttpHeaders.AUTHORIZATION, ACCESS_TOKEN)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpectAll(status().isOk())
+                .andExpect(jsonPath("$.hasNext").value(true))
+                .andExpect(jsonPath("$.trips").isNotEmpty());
     }
 
     @Test
