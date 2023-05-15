a49e012b4ec4218780186a1e2a03e3a1675a4a34
pia2011 <kks01650165@gmail.com>
2023-05-16 01:26:10 +0900

    [TRL-240] feat: ConnectionProxyHandler 구현

---
+++ src/test/java/com/cosain/trilo/common/logging/query/ConnectionProxyHandlerTest.java
@@ -0,0 +1,43 @@
+package com.cosain.trilo.common.logging.query;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.lang.reflect.Method;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class ConnectionProxyHandlerTest {
+
+    @Mock
+    private Connection connection;
+    @Mock
+    private QueryCounter queryCounter;
+    @Mock
+    private PreparedStatement preparedStatement;
+
+    @Test
+    @DisplayName("invoke() 실행 시 PreparedStatement 타입의 객체가 반환된다")
+    void testInvoke_PreparedStatementProxy_Returned() throws Throwable{
+
+        // given
+        given(connection.prepareStatement(anyString())).willReturn(preparedStatement);
+        ConnectionProxyHandler connectionProxyHandler = new ConnectionProxyHandler(connection, queryCounter);
+        Method method = Connection.class.getMethod("prepareStatement", String.class);
+
+        // when
+        Object invoke = connectionProxyHandler.invoke(connection, method, new String[]{""});
+
+        // then
+        Assertions.assertInstanceOf(PreparedStatement.class, invoke);
+    }
+
+}
