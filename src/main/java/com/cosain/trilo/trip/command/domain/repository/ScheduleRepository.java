44f7b397153c60da8a97defb82414d009411eb95
ttasjwi <ttasjwi920@gmail.com>
2023-05-16 21:37:24 +0900

    [TRL-246] feat: Day들의 일정을 임시 보관함으로 일괄 이동하는 쿼리 작성 및 테스트

--- src/main/java/com/cosain/trilo/trip/command/domain/repository/ScheduleRepository.java
+++ src/main/java/com/cosain/trilo/trip/command/domain/repository/ScheduleRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
@@ -30,4 +31,33 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
             "WHERE s.trip.id = :tripId " +
             "  AND ((:dayId is not null AND s.day.id = :dayId) OR (:dayId is null AND s.day is null))")
     int relocateDaySchedules(@Param("tripId") Long tripId, @Param("dayId") Long dayId);
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = "WITH subquery1 AS (" + // 날짜 순 오름차순(같으면 schedule_index 오름차순) 가져오고, 각 행에 순서 번호 부여
+                    "   SELECT " +
+                    "       s2.schedule_id, " +
+                    "       ROW_NUMBER() OVER (ORDER BY d.trip_date ASC, s2.schedule_index ASC) AS row_num " +
+                    "   FROM " +
+                    "       schedules s2 " +
+                    "       JOIN days d ON s2.day_id = d.day_id " +
+                    "   WHERE " +
+                    "       s2.day_id IN :dayIds" +
+                    "), " +
+                    "subquery2 AS (" +
+                    "   SELECT " +
+                    "       MAX(s3.schedule_index) AS max_temporary_storage_schedule_index " + // 임시보관함의 최대 인덱스값 구하기
+                    "   FROM " +
+                    "       schedules s3 " +
+                    "   WHERE " +
+                    "       s3.trip_id = :tripId AND s3.day_id IS NULL" +
+                    ") " +
+                    "UPDATE " +
+                    "   schedules s1 " +
+                    "SET " + // 임시보관함의 제일 뒤로 옮기기
+                    "   s1.day_id = NULL, " +
+                    "   s1.schedule_index = COALESCE((SELECT max_temporary_storage_schedule_index FROM subquery2), -10000000) + " +
+                    "       (SELECT row_num * 10000000 FROM subquery1 WHERE subquery1.schedule_id = s1.schedule_id) " +
+                    "WHERE " +
+                    "   s1.day_id IN :dayIds", nativeQuery = true)
+    int moveSchedulesToTemporaryStorage(@Param("tripId") Long tripId, @Param("dayIds") List<Long> dayIds);
 }
