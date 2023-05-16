3ab3aed1c80a8531653461198b99c4c2ff89d5ad
pia2011 <kks01650165@gmail.com>
2023-05-17 00:05:08 +0900

    [TRL-152] feat: 임시보관함 조회 Repository 기능 구현 및 테스트

--- src/main/java/com/cosain/trilo/trip/query/infra/repository/schedule/ScheduleQueryRepositoryImpl.java
+++ src/main/java/com/cosain/trilo/trip/query/infra/repository/schedule/ScheduleQueryRepositoryImpl.java
@@ -5,8 +5,12 @@ import com.cosain.trilo.trip.query.domain.repository.ScheduleQueryRepository;
 import com.cosain.trilo.trip.query.infra.dto.ScheduleDetail;
 import com.cosain.trilo.trip.query.infra.repository.schedule.jpa.ScheduleQueryJpaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -21,4 +25,12 @@ public class ScheduleQueryRepositoryImpl implements ScheduleQueryRepository {
 
         return scheduleDetail.isEmpty() ? Optional.empty() : Optional.of(ScheduleDto.from(scheduleDetail.get()));
     }
+
+    @Override
+    public Slice<ScheduleDto> findTemporaryScheduleListByTripId(Long tripId, Pageable pageable) {
+        Slice<ScheduleDetail> scheduleDetails = scheduleQueryJpaRepository.findTemporaryScheduleListByTripId(tripId, pageable);
+        List<ScheduleDto> scheduleDtos = scheduleDetails.map(ScheduleDto::from).getContent();
+        return new SliceImpl<>(scheduleDtos, pageable, scheduleDetails.hasNext());
+    }
 }
+
--- src/main/java/com/cosain/trilo/trip/query/infra/repository/schedule/jpa/ScheduleQueryJpaRepositoryImpl.java
+++ src/main/java/com/cosain/trilo/trip/query/infra/repository/schedule/jpa/ScheduleQueryJpaRepositoryImpl.java
@@ -3,8 +3,12 @@ package com.cosain.trilo.trip.query.infra.repository.schedule.jpa;
 import com.cosain.trilo.trip.command.domain.entity.QSchedule;
 import com.cosain.trilo.trip.query.infra.dto.QScheduleDetail;
 import com.cosain.trilo.trip.query.infra.dto.ScheduleDetail;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 import java.util.Optional;
 
@@ -22,5 +26,21 @@ public class ScheduleQueryJpaRepositoryImpl implements ScheduleQueryJpaRepositor
                 .from(schedule)
                 .where(schedule.id.eq(scheduleId))
                 .fetchOne());
+    }
+
+    @Override
+    public Slice<ScheduleDetail> findTemporaryScheduleListByTripId(Long tripId, Pageable pageable) {
+        JPAQuery<ScheduleDetail> jpaQuery = query.select(new QScheduleDetail(schedule.id, schedule.day.id, schedule.title, schedule.place.placeName, schedule.place.coordinate.latitude, schedule.place.coordinate.longitude, schedule.scheduleIndex.value, schedule.content))
+                .from(schedule)
+                .where(schedule.trip.id.eq(tripId).and(schedule.day.id.isNull()))
+                .orderBy(schedule.scheduleIndex.value.asc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize());
+
+        long totalCount = query.from(schedule)
+                .where(schedule.trip.id.eq(tripId).and(schedule.day.id.isNull()))
+                .fetchCount();
+
+        return new PageImpl<>(jpaQuery.fetch(), pageable, totalCount);
     }
 }
