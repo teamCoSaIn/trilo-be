b83bc3d94f0ff978be3ef2206e64a83186da4fa0
pia2011 <kks01650165@gmail.com>
2023-05-19 15:19:02 +0900

    [TRL-154] feat: Day 단건 조회 응용 서비스 구현

---
+++ src/main/java/com/cosain/trilo/trip/query/application/exception/DayNotFoundException.java
@@ -0,0 +1,35 @@
+package com.cosain.trilo.trip.query.application.exception;
+
+import com.cosain.trilo.common.exception.CustomException;
+import org.springframework.http.HttpStatus;
+
+public class DayNotFoundException extends CustomException {
+
+    private static final String ERROR_NAME = "DayNotFoundException";
+    private static final HttpStatus HTTP_STATUS = HttpStatus.NOT_FOUND;
+
+    public DayNotFoundException() {
+    }
+
+    public DayNotFoundException(String debugMessage) {
+        super(debugMessage);
+    }
+
+    public DayNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public DayNotFoundException(String debugMessage, Throwable cause) {
+        super(debugMessage, cause);
+    }
+
+    @Override
+    public String getErrorName() {
+        return ERROR_NAME;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return HTTP_STATUS;
+    }
+}
