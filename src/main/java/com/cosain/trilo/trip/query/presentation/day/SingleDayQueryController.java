1943f11a715aef9ac1820a7584dceea9f87afcf3
pia2011 <kks01650165@gmail.com>
2023-05-19 16:32:48 +0900

    [TRL-154] feat: Day 단건 조회 Controller 구현

--- src/main/java/com/cosain/trilo/trip/query/presentation/day/SingleDayQueryController.java
+++ src/main/java/com/cosain/trilo/trip/query/presentation/day/SingleDayQueryController.java
@@ -1,20 +1,26 @@
 package com.cosain.trilo.trip.query.presentation.day;
 
 import com.cosain.trilo.common.exception.NotImplementedException;
+import com.cosain.trilo.trip.query.application.usecase.DaySearchUseCase;
+import com.cosain.trilo.trip.query.infra.dto.DayScheduleDetail;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * TODO: 일정 단건 조회
- */
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class SingleDayQueryController {
 
+    private final DaySearchUseCase daySearchUseCase;
+
     @GetMapping("/api/days/{dayId}")
-    public String findSingleDay(@PathVariable Long dayId) {
-        throw new NotImplementedException("Day 단건 조회 미구현");
+    @ResponseStatus(HttpStatus.OK)
+    public DayScheduleDetail findSingleDay(@PathVariable Long dayId) {
+        return daySearchUseCase.searchDeySchedule(dayId);
     }
 }
--- src/test/java/com/cosain/trilo/unit/trip/query/presentation/api/day/SingleDayQueryControllerTest.java
+++ src/test/java/com/cosain/trilo/unit/trip/query/presentation/api/day/SingleDayQueryControllerTest.java
@@ -1,13 +1,24 @@
 package com.cosain.trilo.unit.trip.query.presentation.api.day;
 
 import com.cosain.trilo.support.RestControllerTest;
+import com.cosain.trilo.trip.query.application.usecase.DaySearchUseCase;
+import com.cosain.trilo.trip.query.infra.dto.DayScheduleDetail;
 import com.cosain.trilo.trip.query.presentation.day.SingleDayQueryController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -17,15 +28,29 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(SingleDayQueryController.class)
 class SingleDayQueryControllerTest extends RestControllerTest {
 
+    @MockBean
+    private DaySearchUseCase daySearchUseCase;
+    private final String ACCESS_TOKEN = "Bearer accessToken";
     @Test
     @DisplayName("인증된 사용자 요청 -> 미구현 500")
-    @WithMockUser
     public void findSingleSchedule_with_authorizedUser() throws Exception {
-        mockMvc.perform(get("/api/days/1"))
-                .andDo(print())
-                .andExpect(status().isInternalServerError())
-                .andExpect(jsonPath("$.errorCode").exists())
-                .andExpect(jsonPath("$.errorMessage").exists());
+
+        mockingForLoginUserAnnotation();
+        DayScheduleDetail.ScheduleSummary scheduleSummary1 = new DayScheduleDetail.ScheduleSummary(1L, "제목", "장소 이름", 33.33, 33.33);
+        DayScheduleDetail.ScheduleSummary scheduleSummary2 = new DayScheduleDetail.ScheduleSummary(2L, "제목2", "장소 이름2", 33.33, 33.33);
+
+        DayScheduleDetail dayScheduleDetail = new DayScheduleDetail(1L, 1L, LocalDate.of(2023, 2, 3), List.of(scheduleSummary1, scheduleSummary2));
+        given(daySearchUseCase.searchDeySchedule(eq(1L))).willReturn(dayScheduleDetail);
+
+        mockMvc.perform(get("/api/days/1")
+                        .header(HttpHeaders.AUTHORIZATION, ACCESS_TOKEN)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.dayId").value(dayScheduleDetail.getDayId()))
+                .andExpect(jsonPath("$.date").value(dayScheduleDetail.getDate().toString()))
+                .andExpect(jsonPath("$.tripId").value(dayScheduleDetail.getTripId()))
+                .andExpect(jsonPath("$.scheduleSummaries").isArray());
     }
 
     @Test
