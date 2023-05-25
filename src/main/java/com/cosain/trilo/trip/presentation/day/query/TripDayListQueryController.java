c69b5bc29265de661ba62d51cb19b6e6f933e60f
pia2011 <kks01650165@gmail.com>
2023-05-25 15:56:35 +0900

    [TRL-275] refactor: 불필요한 코드 제거

--- src/main/java/com/cosain/trilo/trip/presentation/day/query/TripDayListQueryController.java
+++ src/main/java/com/cosain/trilo/trip/presentation/day/query/TripDayListQueryController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * TODO: 여행의 일정 목록 조회
- */
 @Slf4j
 @RestController
 public class TripDayListQueryController {
