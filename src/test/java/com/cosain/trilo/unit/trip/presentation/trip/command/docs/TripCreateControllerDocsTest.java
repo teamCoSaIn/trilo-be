751d5847050efb6f273efc94f51c9698fca0f362
ttasjwi <88282356+ttasjwi@users.noreply.github.com>
2023-06-02 16:12:23 +0900

    [TRL-287] docs: 여행 생성 API 문서 구조 변경 (#75)
    * [TRL-287] feat: Rest DOCS 커스텀 스니펫 추가
    
    - Spring REST DOCS의 기본 사양으로 제공되는 snippet은 파라미터의 필수값 여부 등을 제공할 수 없는 한계가 존재함.
    - 또, 기본으로 제공되는 칼럼명만 사용할 수 있는 한계가 존재한다.
    - 하지만 src/test/resources/org/spring/framework/restdocs/templates 아래에 커스텀 스니펫들을 두면, 이 스니펫을 기반으로 api 스니펫들이 구성할 수 있다. 칼럼명도 커스텀하게 지정할 수 있다.
    
    * [TRL-287] docs: 여행 생성 Docs 테스트 - 제약조건 추가
    
    * [TRL-287] docs: 여행 생성 API Docs - adoc 파일 구조 수정
    
    - 기존에 `operation::` 문법을 통해 일괄적으로 스니펫을 불러오는 방식을 사용했는데, 좋은 API 문서 제공을 위해서는 이들을 적재적소에 나누고 그 위치마다 적절한 설명이 있으면 각 위치마다 덧붙이는 식으로 사용하는 것이 나아보였다.

--- src/test/java/com/cosain/trilo/unit/trip/presentation/trip/command/docs/TripCreateControllerDocsTest.java
+++ src/test/java/com/cosain/trilo/unit/trip/presentation/trip/command/docs/TripCreateControllerDocsTest.java
@@ -19,11 +19,14 @@ import java.nio.charset.StandardCharsets;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.data.redis.connection.DataType.STRING;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
+import static org.springframework.restdocs.payload.JsonFieldType.STRING;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TripCreateController.class)
@@ -41,13 +44,14 @@ public class TripCreateControllerDocsTest extends RestDocsTestSupport {
 
     @Test
     @DisplayName("인증된 사용자의 여행 생성 요청 -> 성공")
-    void successTest() throws Exception{
+    void successTest() throws Exception {
         mockingForLoginUserAnnotation();
 
         String rawTitle = "제목";
+        Long tripId = 1L;
         TripCreateRequest request = new TripCreateRequest(rawTitle);
         given(tripCreateCommandFactory.createCommand(eq(rawTitle))).willReturn(new TripCreateCommand(TripTitle.of(rawTitle)));
-        given(tripCreateUseCase.createTrip(any(), any(TripCreateCommand.class))).willReturn(1L);
+        given(tripCreateUseCase.createTrip(any(), any(TripCreateCommand.class))).willReturn(tripId);
 
         mockMvc.perform(post(BASE_URL)
                         .header(HttpHeaders.AUTHORIZATION, ACCESS_TOKEN)
@@ -55,16 +59,23 @@ public class TripCreateControllerDocsTest extends RestDocsTestSupport {
                         .characterEncoding(StandardCharsets.UTF_8)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.tripId").value(tripId))
                 .andDo(restDocs.document(
-                    requestHeaders(
-                            headerWithName(HttpHeaders.AUTHORIZATION).description("Bearer 타입 AccessToken")
-                    ),
-                    requestFields(
-                            fieldWithPath("title").type(STRING).description("제목")
-                    ),
-                    responseFields(
-                            fieldWithPath("tripId").type(STRING).description("생성된 여행의 식별자 ID")
-                    )
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION)
+                                        .description("Bearer 타입 AccessToken")
+                        ),
+                        requestFields(
+                                fieldWithPath("title")
+                                        .type(STRING)
+                                        .description("여행의 제목")
+                                        .attributes(key("constraints").value("null 또는 공백일 수 없으며, 길이는 1-20자까지만 허용됩니다."))
+                        ),
+                        responseFields(
+                                fieldWithPath("tripId")
+                                        .type(NUMBER)
+                                        .description("생성된 여행의 식별자(id)")
+                        )
                 ));
     }
 }
