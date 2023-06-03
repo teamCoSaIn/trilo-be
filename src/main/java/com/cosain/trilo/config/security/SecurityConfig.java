c3195dc049d263dd48811ce65d47a725e94ddecf
pia2011 <53935439+pia2011@users.noreply.github.com>
2023-06-03 17:24:43 +0900

    [TRL-290] feat: preflight request 모두 허용 (#78)

--- src/main/java/com/cosain/trilo/config/security/SecurityConfig.java
+++ src/main/java/com/cosain/trilo/config/security/SecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
@@ -51,6 +52,7 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests(request -> request
+                        .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                         .requestMatchers(antMatcher("/h2-console/**")).permitAll()
                         .requestMatchers(antMatcher(HttpMethod.GET, "/deploy/**")).permitAll()
                         .requestMatchers(antMatcher(HttpMethod.GET, "/api/auth/token/refresh-token-info")).permitAll()
