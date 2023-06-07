ec340bd90996c43f97d587161401572c116a6661
pia2011 <kks01650165@gmail.com>
2023-06-07 11:23:20 +0900

    [TRL-291] chore: 불필요한 외부 라이브러리 의존성 제거 -> spring 지원 라이브러리 사용

--- src/main/java/com/cosain/trilo/config/security/HttpCookieOAuth2AuthorizationRequestRepository.java
+++ src/main/java/com/cosain/trilo/config/security/HttpCookieOAuth2AuthorizationRequestRepository.java
@@ -1,12 +1,12 @@
 package com.cosain.trilo.config.security;
 
 import com.cosain.trilo.config.security.util.CookieUtil;
-import com.nimbusds.oauth2.sdk.util.StringUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 @Component
 public class HttpCookieOAuth2AuthorizationRequestRepository implements AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
@@ -31,7 +31,7 @@ public class HttpCookieOAuth2AuthorizationRequestRepository implements Authoriza
 
         CookieUtil.addCookie(response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME, CookieUtil.serialize(authorizationRequest), cookieExpireSeconds);
         String redirectUriAfterLogin = request.getParameter(REDIRECT_URI_PARAM_COOKIE_NAME);
-        if (StringUtils.isNotBlank(redirectUriAfterLogin)) {
+        if (StringUtils.hasText(redirectUriAfterLogin)) {
             CookieUtil.addCookie(response, REDIRECT_URI_PARAM_COOKIE_NAME, redirectUriAfterLogin, cookieExpireSeconds);
         }
     }
