06b23f89fc5b823d6f30762d66ed56a9bcc3ad69
ttasjwi <ttasjwi920@gmail.com>
2023-06-14 22:27:15 +0900

    [TRL-307] feat: DayColor를 랜덤하게 생성하는 메서드 작성 및 테스트

--- src/main/java/com/cosain/trilo/trip/domain/vo/DayColor.java
+++ src/main/java/com/cosain/trilo/trip/domain/vo/DayColor.java
@@ -4,22 +4,36 @@ package com.cosain.trilo.trip.domain.vo;
 import com.cosain.trilo.trip.domain.exception.InvalidDayColorNameException;
 import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 @Getter
 public enum DayColor {
 
-    RED("#FB6C6C"),
-    ORANGE("#F4A17D"),
-    LIGHT_GREEN("#B9F15D"),
-    GREEN("#43D65A"),
-    BLUE("#4D77FF"),
-    PURPLE("#D96FF8"),
-    VIOLET("#8F57FB"),
-    BLACK("#383B40");
+    RED(0, "#FB6C6C"),
+    ORANGE(1, "#F4A17D"),
+    LIGHT_GREEN(2, "#B9F15D"),
+    GREEN(3, "#43D65A"),
+    BLUE(4, "#4D77FF"),
+    PURPLE(5, "#D96FF8"),
+    VIOLET(6, "#8F57FB"),
+    BLACK(7, "#383B40");
 
+    private static final Map<Integer, DayColor> DAY_COLORS = Arrays
+            .stream(values())
+            .collect(Collectors.toUnmodifiableMap(DayColor::getId, Function.identity()));
+
+    private static final int COLOR_COUNT = DAY_COLORS.size();
+
+    private final int id;
     private final String value;
 
-    DayColor(String value) {
+    DayColor(int id, String value) {
         this.value = value;
+        this.id = id;
     }
 
     public static DayColor of(String name) {
@@ -28,6 +42,11 @@ public enum DayColor {
         } catch (NullPointerException | IllegalArgumentException e) {
             throw new InvalidDayColorNameException("색상 이름이 null 이거나 지원되지 않는 이름임", e);
         }
+    }
+
+    public static DayColor random(Random random) {
+        int randomDayColorId = random.nextInt(COLOR_COUNT);
+        return DAY_COLORS.get(randomDayColorId);
     }
 
     @Override
--- src/test/java/com/cosain/trilo/unit/trip/domain/vo/DayColorTest.java
+++ src/test/java/com/cosain/trilo/unit/trip/domain/vo/DayColorTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Random;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -152,4 +154,132 @@ public class DayColorTest {
             assertThat(dayColor.getValue()).isEqualTo("#383B40");
         }
     }
+
+
+    @Nested
+    @DisplayName("random 메서드를 통해 랜덤한 DayColor를 얻어올 때")
+    public class RandomTest {
+
+        // 항상 해당 값을 반환하는 Random
+        private Random stubRandom(int returnValue) {
+            return new Random() {
+                @Override
+                public int nextInt(int bound) {
+                    return returnValue;
+                }
+            };
+        }
+        @Test
+        @DisplayName("Random이 0 발생 -> RED DayColor 반환")
+        public void testRed() {
+            // given
+            Random random = stubRandom(0);
+
+            // when
+            DayColor dayColor = DayColor.random(random);
+
+            // then
+            assertThat(dayColor).isSameAs(DayColor.RED);
+            assertThat(dayColor.getValue()).isEqualTo("#FB6C6C");
+        }
+
+        @Test
+        @DisplayName("Random이 1 발생 -> ORANGE DayColor 반환")
+        public void testOrange() {
+            // given
+            Random random = stubRandom(1);
+
+            // when
+            DayColor dayColor = DayColor.random(random);
+
+            // then
+            assertThat(dayColor).isSameAs(DayColor.ORANGE);
+            assertThat(dayColor.getValue()).isEqualTo("#F4A17D");
+        }
+
+        @Test
+        @DisplayName("Random이 2 발생 -> LIGHT_GREEN DayColor 반환")
+        public void testLightGreen() {
+            // given
+            Random random = stubRandom(2);
+
+            // when
+            DayColor dayColor = DayColor.random(random);
+
+            // then
+            assertThat(dayColor).isSameAs(DayColor.LIGHT_GREEN);
+            assertThat(dayColor.getValue()).isEqualTo("#B9F15D");
+        }
+
+        @Test
+        @DisplayName("Random이 3 발생 -> GREEN DayColor 반환")
+        public void testGreen() {
+            // given
+            Random random = stubRandom(3);
+
+            // when
+            DayColor dayColor = DayColor.random(random);
+
+            // then
+            assertThat(dayColor).isSameAs(DayColor.GREEN);
+            assertThat(dayColor.getValue()).isEqualTo("#43D65A");
+        }
+
+        @Test
+        @DisplayName("Random이 4 발생 -> BLUE DayColor 생성")
+        public void testBlue() {
+            // given
+            Random random = stubRandom(4);
+
+            // when
+            DayColor dayColor = DayColor.random(random);
+
+            // then
+            assertThat(dayColor).isSameAs(DayColor.BLUE);
+            assertThat(dayColor.getValue()).isEqualTo("#4D77FF");
+        }
+
+        @Test
+        @DisplayName("Random이 5 발생 -> PURPLE DayColor 생성")
+        public void testPurple() {
+            // given
+            Random random = stubRandom(5);
+
+            // when
+            DayColor dayColor = DayColor.random(random);
+
+            // then
+            assertThat(dayColor).isSameAs(DayColor.PURPLE);
+            assertThat(dayColor.getValue()).isEqualTo("#D96FF8");
+        }
+
+        @Test
+        @DisplayName("Random이 6 발생 -> VIOLET DayColor 생성")
+        public void testViolet() {
+            // given
+            Random random = stubRandom(6);
+
+            // when
+            DayColor dayColor = DayColor.random(random);
+
+            // then
+            assertThat(dayColor).isSameAs(DayColor.VIOLET);
+            assertThat(dayColor.getValue()).isEqualTo("#8F57FB");
+        }
+
+        @Test
+        @DisplayName("Random이 7 발생 -> BLACK DayColor 생성")
+        public void testBlack() {
+            // given
+            Random random = stubRandom(7);
+
+            // when
+            DayColor dayColor = DayColor.random(random);
+
+            // then
+            assertThat(dayColor).isSameAs(DayColor.BLACK);
+            assertThat(dayColor.getValue()).isEqualTo("#383B40");
+        }
+    }
+
 }
