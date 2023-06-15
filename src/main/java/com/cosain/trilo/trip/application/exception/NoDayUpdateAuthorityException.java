a27ece74084859300ac1c94c74e8bffcee064178
ttasjwi <ttasjwi920@gmail.com>
2023-06-15 22:46:25 +0900

    [TRL-310] feat: DayColorUpdateService 기능 구현 및 테스트 작성

---
+++ src/main/java/com/cosain/trilo/trip/application/exception/NoDayUpdateAuthorityException.java
@@ -0,0 +1,36 @@
+package com.cosain.trilo.trip.application.exception;
+
+import com.cosain.trilo.common.exception.CustomException;
+import org.springframework.http.HttpStatus;
+
+public class NoDayUpdateAuthorityException extends CustomException {
+
+    private static final String ERROR_CODE = "day-0006";
+    private static final HttpStatus HTTP_STATUS = HttpStatus.FORBIDDEN;
+
+    public NoDayUpdateAuthorityException() {
+        super();
+    }
+
+    public NoDayUpdateAuthorityException(String debugMessage) {
+        super(debugMessage);
+    }
+
+    public NoDayUpdateAuthorityException(Throwable cause) {
+        super(cause);
+    }
+
+    public NoDayUpdateAuthorityException(String debugMessage, Throwable cause) {
+        super(debugMessage, cause);
+    }
+
+    @Override
+    public String getErrorCode() {
+        return ERROR_CODE;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return HTTP_STATUS;
+    }
+}
