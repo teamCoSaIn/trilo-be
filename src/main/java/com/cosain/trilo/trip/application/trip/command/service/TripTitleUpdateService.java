c2d0ebc72d672440d48e8e3653f6fbc651874ec0
ttasjwi <ttasjwi920@gmail.com>
2023-06-16 15:23:12 +0900

    [TRL-309] feat: 여행 제목 변경 서비스 구현 및 테스트

---
+++ src/main/java/com/cosain/trilo/trip/application/trip/command/service/TripTitleUpdateService.java
@@ -0,0 +1,39 @@
+package com.cosain.trilo.trip.application.trip.command.service;
+
+import com.cosain.trilo.trip.application.exception.NoTripUpdateAuthorityException;
+import com.cosain.trilo.trip.application.exception.TripNotFoundException;
+import com.cosain.trilo.trip.application.trip.command.usecase.TripTitleUpdateUseCase;
+import com.cosain.trilo.trip.application.trip.command.usecase.dto.TripTitleUpdateCommand;
+import com.cosain.trilo.trip.domain.entity.Trip;
+import com.cosain.trilo.trip.domain.repository.TripRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class TripTitleUpdateService implements TripTitleUpdateUseCase {
+
+    private final TripRepository tripRepository;
+
+    @Override
+    @Transactional
+    public void updateTripTitle(Long tripId, Long tripperId, TripTitleUpdateCommand updateCommand) {
+        Trip trip = findTrip(tripId);
+        validateTripUpdateAuthority(trip, tripperId);
+
+        trip.changeTitle(updateCommand.getTripTitle());
+    }
+
+    private Trip findTrip(Long tripId) {
+        return tripRepository.findById(tripId)
+                .orElseThrow(() -> new TripNotFoundException("일치하는 식별자의 Trip을 찾을 수 없음"));
+    }
+
+    private void validateTripUpdateAuthority(Trip trip, Long tripperId) {
+        if (!trip.getTripperId().equals(tripperId)) {
+            throw new NoTripUpdateAuthorityException("여행을 수정할 권한이 없는 사람이 수정하려고 시도함");
+        }
+    }
+
+}
