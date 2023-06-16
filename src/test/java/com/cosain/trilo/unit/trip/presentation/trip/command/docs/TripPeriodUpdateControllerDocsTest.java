0fd4fc4a03181fdf5ee377b8284c46488ad4fcdf
ttasjwi <ttasjwi920@gmail.com>
2023-06-16 16:27:39 +0900

    [TRL-314] test: 여행 기간 수정 API - 문서화 테스트

--- src/test/java/com/cosain/trilo/unit/trip/presentation/trip/command/docs/TripPeriodUpdateControllerDocsTest.java
+++ src/test/java/com/cosain/trilo/unit/trip/presentation/trip/command/docs/TripPeriodUpdateControllerDocsTest.java
@@ -1,12 +1,12 @@
 package com.cosain.trilo.unit.trip.presentation.trip.command.docs;
 
 import com.cosain.trilo.support.RestDocsTestSupport;
-import com.cosain.trilo.trip.application.trip.command.usecase.TripTitleUpdateUseCase;
-import com.cosain.trilo.trip.application.trip.command.usecase.dto.TripTitleUpdateCommand;
-import com.cosain.trilo.trip.application.trip.command.usecase.dto.factory.TripTitleUpdateCommandFactory;
-import com.cosain.trilo.trip.domain.vo.TripTitle;
-import com.cosain.trilo.trip.presentation.trip.command.TripTitleUpdateController;
-import com.cosain.trilo.trip.presentation.trip.command.dto.request.TripTitleUpdateRequest;
+import com.cosain.trilo.trip.application.trip.command.usecase.TripPeriodUpdateUseCase;
+import com.cosain.trilo.trip.application.trip.command.usecase.dto.TripPeriodUpdateCommand;
+import com.cosain.trilo.trip.application.trip.command.usecase.dto.factory.TripPeriodUpdateCommandFactory;
+import com.cosain.trilo.trip.domain.vo.TripPeriod;
+import com.cosain.trilo.trip.presentation.trip.command.TripPeriodUpdateController;
+import com.cosain.trilo.trip.presentation.trip.command.dto.request.TripPeriodUpdateRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -15,6 +15,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -35,34 +36,35 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(TripTitleUpdateController.class)
-@DisplayName("여행 제목 수정 API DOCS 테스트")
-public class TripTitleUpdateControllerDocsTest extends RestDocsTestSupport {
+@WebMvcTest(TripPeriodUpdateController.class)
+@DisplayName("여행 기간 수정 API DOCS 테스트")
+public class TripPeriodUpdateControllerDocsTest extends RestDocsTestSupport {
 
     @MockBean
-    private TripTitleUpdateUseCase tripTitleUpdateUseCase;
+    private TripPeriodUpdateUseCase tripPeriodUpdateUseCase;
 
     @MockBean
-    private TripTitleUpdateCommandFactory tripTitleUpdateCommandFactory;
+    private TripPeriodUpdateCommandFactory tripPeriodUpdateCommandFactory;
 
     private final String ACCESS_TOKEN = "Bearer accessToken";
 
     @Test
     @DisplayName("인증된 사용자 요청 -> 성공")
-    public void updateTripTitle_with_authorizedUser() throws Exception {
+    public void updateTripPeriod_with_authorizedUser() throws Exception {
         // given
         mockingForLoginUserAnnotation();
 
         Long tripId = 1L;
-        String rawTitle = "변경할 제목";
+        LocalDate startDate = LocalDate.of(2023,4,1);
+        LocalDate endDate = LocalDate.of(2023,4,5);
 
-        TripTitleUpdateRequest request = new TripTitleUpdateRequest(rawTitle);
+        TripPeriodUpdateRequest request = new TripPeriodUpdateRequest(startDate, endDate);
+        TripPeriodUpdateCommand command = new TripPeriodUpdateCommand(TripPeriod.of(startDate, endDate));
 
-        given(tripTitleUpdateCommandFactory.createCommand(eq(rawTitle)))
-                .willReturn(new TripTitleUpdateCommand(TripTitle.of(rawTitle)));
-        willDoNothing().given(tripTitleUpdateUseCase).updateTripTitle(eq(tripId), any(), any(TripTitleUpdateCommand.class));
+        given(tripPeriodUpdateCommandFactory.createCommand(eq(startDate), eq(endDate))).willReturn(command);
+        willDoNothing().given(tripPeriodUpdateUseCase).updateTripPeriod(eq(tripId), any(), any(TripPeriodUpdateCommand.class));
 
-        mockMvc.perform(put("/api/trips/{tripId}/title", tripId)
+        mockMvc.perform(put("/api/trips/{tripId}/period", tripId)
                         .header(HttpHeaders.AUTHORIZATION, ACCESS_TOKEN)
                         .content(createJson(request))
                         .characterEncoding(StandardCharsets.UTF_8)
@@ -70,30 +72,35 @@ public class TripTitleUpdateControllerDocsTest extends RestDocsTestSupport {
                 )
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.tripId").value(tripId))
-                .andDo(restDocs.document(
+                .andExpect(jsonPath("$.tripId").value(tripId)) .andDo(restDocs.document(
                         requestHeaders(
                                 headerWithName(HttpHeaders.AUTHORIZATION)
                                         .description("Bearer 타입 AccessToken")
                         ),
                         pathParameters(
                                 parameterWithName("tripId")
-                                        .description("제목 수정할 여행 ID")
+                                        .description("기간 수정할 여행 ID")
                         ),
                         requestFields(
-                                fieldWithPath("title")
+                                fieldWithPath("startDate")
                                         .type(STRING)
-                                        .description("여행 제목")
-                                        .attributes(key("constraints").value("null 또는 공백일 수 없으며, 길이는 1-20자까지만 허용됩니다."))
+                                        .optional()
+                                        .description("여행 시작 일자 (형식 : yyyy-MM-dd)")
+                                        .attributes(key("constraints").value("startDate,endDate는 한쪽만 null이여선 안 되며(둘다 null은 가능), endDate가 startDate보다 앞서선 안 됩니다. 여행 일수는 최대 10일까지 허용됩니다.")),
+                                fieldWithPath("endDate")
+                                        .type(STRING)
+                                        .optional()
+                                        .description("여행 종료 일자 (형식 : yyyy-MM-dd)")
+                                        .attributes(key("constraints").value("startDate 참고"))
                         ),
                         responseFields(
                                 fieldWithPath("tripId")
                                         .type(NUMBER)
-                                        .description("수정된 여행 식별자(id)")
+                                        .description("기간 수정된 여행 식별자(id)")
                         )
                 ));
 
-        verify(tripTitleUpdateUseCase, times(1)).updateTripTitle(eq(tripId), any(), any(TripTitleUpdateCommand.class));
-        verify(tripTitleUpdateCommandFactory, times(1)).createCommand(eq(rawTitle));
+        verify(tripPeriodUpdateUseCase, times(1)).updateTripPeriod(eq(tripId), any(), any(TripPeriodUpdateCommand.class));
+        verify(tripPeriodUpdateCommandFactory, times(1)).createCommand(eq(startDate), eq(endDate));
     }
 }
