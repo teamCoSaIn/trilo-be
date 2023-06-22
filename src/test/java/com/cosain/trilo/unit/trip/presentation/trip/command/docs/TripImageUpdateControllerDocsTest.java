d2e261f923ce6ac96acf2bf8ea4f2174e12effe4
ttasjwi <ttasjwi920@gmail.com>
2023-06-22 12:13:40 +0900

    [TRL-324] docs: 여행 이미지 수정 API 문서화 테스트

---
+++ src/test/java/com/cosain/trilo/unit/trip/presentation/trip/command/docs/TripImageUpdateControllerDocsTest.java
@@ -0,0 +1,99 @@
+package com.cosain.trilo.unit.trip.presentation.trip.command.docs;
+
+import com.cosain.trilo.common.file.ImageFile;
+import com.cosain.trilo.support.RestDocsTestSupport;
+import com.cosain.trilo.trip.application.trip.command.usecase.TripImageUpdateUseCase;
+import com.cosain.trilo.trip.presentation.trip.command.TripImageUpdateController;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.FileInputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.multipart;
+import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
+import static org.springframework.restdocs.payload.JsonFieldType.STRING;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TripImageUpdateController.class)
+@DisplayName("여행 이미지 수정 API DOCS 테스트")
+public class TripImageUpdateControllerDocsTest extends RestDocsTestSupport {
+
+    @MockBean
+    private TripImageUpdateUseCase tripImageUpdateUseCase;
+
+    private final static String ACCESS_TOKEN = "Bearer accessToken";
+
+    @Test
+    @DisplayName("jpeg -> 성공")
+    public void tripImageUpdateApiDocsTest() throws Exception {
+        mockingForLoginUserAnnotation();
+        Long tripId = 1L;
+
+        String name = "image";
+        String fileName = "test-jpeg-image.jpeg";
+        String filePath = "src/test/resources/testFiles/" + fileName;
+        FileInputStream fileInputStream = new FileInputStream(filePath);
+        String contentType = "image/jpeg";
+
+        MockMultipartFile multipartFile = new MockMultipartFile(name, fileName, contentType, fileInputStream);
+
+        String imagePath = String.format("https://{이미지 파일 저장소 주소}/trips/%s/{이미지 파일명}.jpeg", tripId);
+        given(tripImageUpdateUseCase.updateTripImage(eq(tripId), any(), any(ImageFile.class)))
+                .willReturn(imagePath);
+
+        mockMvc.perform(multipart("/api/trips/{tripId}/image/update", tripId)
+                        .file(multipartFile)
+                        .header(HttpHeaders.AUTHORIZATION, ACCESS_TOKEN)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.tripId").value(tripId))
+                .andExpect(jsonPath("$.imagePath").value(imagePath))
+                .andDo(restDocs.document(
+                        requestHeaders(headerWithName(HttpHeaders.AUTHORIZATION)
+                                        .description("Bearer 타입 AccessToken")
+                        ),
+                        pathParameters(
+                                parameterWithName("tripId")
+                                        .description("이미지 수정할 여행(Trip)의 식별자(id)")
+                        ),
+                        requestParts(partWithName("image")
+                                .description("올릴 이미지 파일")
+                                .attributes(key("constraints").value("이미지를 필수로 전달해야합니다. 허용되는 이미지 타입(jpg, jpeg, png, gif, webp)"))
+                        ),
+                        responseFields(
+                                fieldWithPath("tripId")
+                                        .type(NUMBER)
+                                        .description("수정된 여행(trip) 식별자(id)"),
+                                fieldWithPath("imagePath")
+                                        .type(STRING)
+                                        .description("이미지가 저장된 경로 URL")
+                        )
+                ));
+
+        verify(tripImageUpdateUseCase, times(1)).updateTripImage(eq(tripId), any(), any(ImageFile.class));
+    }
+
+
+}
