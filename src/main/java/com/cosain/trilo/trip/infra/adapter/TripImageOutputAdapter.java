74d108450ee37ac1e68e25685f0f40efa90996fc
ttasjwi <ttasjwi920@gmail.com>
2023-06-22 16:49:23 +0900

    [TRL-323] feat: TripImageOutputAdapter 구현 및 테스트

--- src/main/java/com/cosain/trilo/trip/infra/adapter/TripImageOutputAdapter.java
+++ src/main/java/com/cosain/trilo/trip/infra/adapter/TripImageOutputAdapter.java
@@ -1,10 +1,15 @@
 package com.cosain.trilo.trip.infra.adapter;
 
+import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.cosain.trilo.common.file.ImageFile;
+import com.cosain.trilo.trip.application.exception.TripImageUploadFailedException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Slf4j
 @Component
@@ -24,7 +29,21 @@ public class TripImageOutputAdapter {
     }
 
     public String uploadImage(ImageFile file, String uploadFileName) {
-        return null;
+        ObjectMetadata objectMetadata = makeMetaData(file);
+        try {
+            amazonS3.putObject(bucketName, uploadFileName, file.getInputStream(), objectMetadata);
+        } catch (SdkClientException e) {
+            log.error("[S3] 여행 이미지 업로드 실패", e);
+            throw new TripImageUploadFailedException("[S3] 여행 이미지 업로드 실패", e);
+        }
+        return bucketPath + uploadFileName;
+    }
+
+    private ObjectMetadata makeMetaData(ImageFile file) {
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(file.getSize());
+        objectMetadata.setContentType(file.getContentType());
+        return objectMetadata;
     }
 
 }
---
+++ src/test/java/com/cosain/trilo/unit/trip/infra/adapter/TripImageOutputAdapterTest.java
@@ -0,0 +1,84 @@
+package com.cosain.trilo.unit.trip.infra.adapter;
+
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.cosain.trilo.common.file.ImageFile;
+import com.cosain.trilo.trip.application.exception.TripImageUploadFailedException;
+import com.cosain.trilo.trip.infra.adapter.TripImageOutputAdapter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@DisplayName("TripImageOutputAdapter 테스트")
+public class TripImageOutputAdapterTest {
+
+    private static final String TEST_RESOURCE_PATH = "src/test/resources/testFiles/";
+
+    private TripImageOutputAdapter tripImageOutputAdapter;
+
+    private AmazonS3 amazonS3;
+     private String bucketName;
+    private String bucketPath;
+
+    @BeforeEach
+    public void setUp() {
+        amazonS3 = mock(AmazonS3.class);
+        bucketName = "bucketName";
+        bucketPath = "https://bucketPath.com/";
+        tripImageOutputAdapter = new TripImageOutputAdapter(amazonS3, bucketName, bucketPath);
+    }
+
+    @Test
+    @DisplayName("이미지 업로드 성공 테스트")
+    void testSuccess() throws Exception{
+        ImageFile imageFile = imageFileFixture("test-jpeg-image.jpeg");
+
+        String fileName = "xxx/{랜덤 uuid}.jpeg";
+
+        // when
+        String fullPath = tripImageOutputAdapter.uploadImage(imageFile, fileName);
+
+        // then
+        verify(amazonS3, times(1)).putObject(eq(bucketName), eq(fileName), any(InputStream.class), any(ObjectMetadata.class));
+        assertThat(fullPath).isEqualTo(bucketPath.concat(fileName));
+    }
+
+    @Test
+    @DisplayName("업로드 과정 SdkClientException 발생 -> TestImageUploadFailedException 발생")
+    void testSdkClientExceptionFailed() throws Exception{
+        ImageFile imageFile = imageFileFixture("test-jpeg-image.jpeg");
+
+        String fileName = "xxx/{랜덤 uuid}.jpeg";
+
+        given(amazonS3.putObject(eq(bucketName),eq(fileName),any(InputStream.class), any(ObjectMetadata.class)))
+                .willThrow(SdkClientException.class);
+
+        // when & then
+        assertThatThrownBy(() -> tripImageOutputAdapter.uploadImage(imageFile, fileName))
+                .isInstanceOf(TripImageUploadFailedException.class);
+        verify(amazonS3, times(1)).putObject(eq(bucketName), eq(fileName), any(InputStream.class), any(ObjectMetadata.class));
+    }
+
+    private ImageFile imageFileFixture(String testImageResourceFileName) throws IOException {
+        String name = "image";
+        String filePath = TEST_RESOURCE_PATH + testImageResourceFileName;
+        FileInputStream fileInputStream = new FileInputStream(filePath);
+        String contentType = "image/jpeg";
+
+        MockMultipartFile multipartFile = new MockMultipartFile(name, testImageResourceFileName, contentType, fileInputStream);
+        return ImageFile.from(multipartFile);
+    }
+}
