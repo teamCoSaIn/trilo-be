77b264710d1ff0345b3859b67dc6a117eb3e7f9a
pia2011 <kks01650165@gmail.com>
2023-06-28 00:13:33 +0900

    [TRL-361] test: 회원 프로필 조회 실패 케이스 테스트 코드 추가

--- src/test/java/com/cosain/trilo/integration/user/UserIntegrationTest.java
+++ src/test/java/com/cosain/trilo/integration/user/UserIntegrationTest.java
@@ -4,10 +4,12 @@ import com.cosain.trilo.support.IntegrationTest;
 import com.cosain.trilo.user.domain.User;
 import com.cosain.trilo.user.domain.UserRepository;
 import org.apache.http.HttpHeaders;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 
+import static com.cosain.trilo.fixture.UserFixture.GOOGLE_MEMBER;
 import static com.cosain.trilo.fixture.UserFixture.KAKAO_MEMBER;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,20 +21,43 @@ public class UserIntegrationTest extends IntegrationTest {
     @Autowired
     UserRepository userRepository;
 
-    @Test
-    void 회원_프로필_조회_성공() throws Exception{
-        // given
-        User user = userRepository.save(KAKAO_MEMBER.create());
-        String accessToken = createAccessToken(user.getId());
+    @Nested
+    class 회원_프로필_조회{
+        @Test
+        void 회원_프로필_조회_성공() throws Exception{
+            // given
+            User user = userRepository.save(KAKAO_MEMBER.create());
+            String accessToken = createAccessToken(user.getId());
 
-        // when & then
-        mockMvc.perform(RestDocumentationRequestBuilders.get(BASE_URL + "/{userId}/profile", user.getId())
-                .header(HttpHeaders.AUTHORIZATION, TOKEN_TYPE + accessToken))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(user.getId()))
-                .andExpect(jsonPath("$.name").value(user.getName()))
-                .andExpect(jsonPath("$.email").value(user.getEmail()))
-                .andExpect(jsonPath("$.profileImageURL").value(user.getProfileImageURL()))
-                .andExpect(jsonPath("$.role").value(user.getRole().name()));
+            // when & then
+            mockMvc.perform(RestDocumentationRequestBuilders.get(BASE_URL + "/{userId}/profile", user.getId())
+                            .header(HttpHeaders.AUTHORIZATION, TOKEN_TYPE + accessToken))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.id").value(user.getId()))
+                    .andExpect(jsonPath("$.name").value(user.getName()))
+                    .andExpect(jsonPath("$.email").value(user.getEmail()))
+                    .andExpect(jsonPath("$.profileImageURL").value(user.getProfileImageURL()))
+                    .andExpect(jsonPath("$.role").value(user.getRole().name()));
+        }
+
+        @Test
+        void 회원_프로필_조회_시_다른_회원의_프로필을_조회할_경우_403_에러를_발생시킨다() throws Exception{
+            // given
+            User user = userRepository.save(KAKAO_MEMBER.create());
+            User anotherUser = userRepository.save(GOOGLE_MEMBER.create());
+            String accessToken = createAccessToken(user.getId());
+
+            // when & then
+            mockMvc.perform(RestDocumentationRequestBuilders.get(BASE_URL + "/{userId}/profile", anotherUser.getId())
+                            .header(HttpHeaders.AUTHORIZATION, TOKEN_TYPE + accessToken))
+                    .andExpect(status().isForbidden())
+                    .andExpect(jsonPath("$.errorCode").value("user-0002"))
+                    .andExpect(jsonPath("$.errorMessage").exists())
+                    .andExpect(jsonPath("$.errorDetail").exists());
+        }
     }
+
+
+
+
 }
