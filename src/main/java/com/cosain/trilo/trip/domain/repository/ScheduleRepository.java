78f51f7a73881602d57de00d7a1c47952dde6c18
ttasjwi <ttasjwi920@gmail.com>
2023-06-27 20:43:30 +0900

    [TRL-362] fix: 빌드테스트 설정 MySQL 기반으로 수정

--- src/main/java/com/cosain/trilo/trip/domain/repository/ScheduleRepository.java
+++ src/main/java/com/cosain/trilo/trip/domain/repository/ScheduleRepository.java
@@ -21,44 +21,50 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     void deleteAllByTripId(@Param("tripId") Long tripId);
 
     @Modifying(clearAutomatically = true)
-    @Query("UPDATE Schedule s " +
-            "SET s.scheduleIndex.value = (" +
-            "  SELECT COUNT(s2) " +
-            "  FROM Schedule s2 " +
-            "  WHERE (:dayId is not null AND s2.day.id = :dayId AND s2.scheduleIndex.value < s.scheduleIndex.value) " +
-            "        OR (:dayId is null AND s2.day is null AND s2.trip.id = :tripId AND s2.scheduleIndex.value < s.scheduleIndex.value) " +
-            ") * 10000000 " +
-            "WHERE s.trip.id = :tripId " +
-            "  AND ((:dayId is not null AND s.day.id = :dayId) OR (:dayId is null AND s.day is null))")
+    @Query(value = """
+            UPDATE schedules s
+            SET s.schedule_index = (
+              SELECT subQuery.countValue * 10000000
+              FROM (
+                SELECT COUNT(s2.schedule_id) AS countValue
+                FROM schedules s2
+                WHERE (:dayId IS NOT NULL AND s2.day_id = :dayId AND s2.schedule_index < s.schedule_index)
+                      OR (:dayId IS NULL AND s2.day_id IS NULL AND s2.trip_id = :tripId AND s2.schedule_index < s.schedule_index)
+              ) AS subQuery
+            )
+            WHERE s.trip_id = :tripId
+              AND ((:dayId IS NOT NULL AND s.day_id = :dayId) OR (:dayId IS NULL AND s.day_id IS NULL))
+           """, nativeQuery = true
+    )
     int relocateDaySchedules(@Param("tripId") Long tripId, @Param("dayId") Long dayId);
 
     @Modifying(clearAutomatically = true)
     @Query(value = "WITH subquery1 AS (" + // 날짜 순 오름차순(같으면 schedule_index 오름차순) 가져오고, 각 행에 순서 번호 부여
-                    "   SELECT " +
-                    "       s2.schedule_id, " +
-                    "       ROW_NUMBER() OVER (ORDER BY d.trip_date ASC, s2.schedule_index ASC) AS row_num " +
-                    "   FROM " +
-                    "       schedules s2 " +
-                    "       JOIN days d ON s2.day_id = d.day_id " +
-                    "   WHERE " +
-                    "       s2.day_id IN :dayIds" +
-                    "), " +
-                    "subquery2 AS (" +
-                    "   SELECT " +
-                    "       MAX(s3.schedule_index) AS max_temporary_storage_schedule_index " + // 임시보관함의 최대 인덱스값 구하기
-                    "   FROM " +
-                    "       schedules s3 " +
-                    "   WHERE " +
-                    "       s3.trip_id = :tripId AND s3.day_id IS NULL" +
-                    ") " +
-                    "UPDATE " +
-                    "   schedules s1 " +
-                    "SET " + // 임시보관함의 제일 뒤로 옮기기
-                    "   s1.day_id = NULL, " +
-                    "   s1.schedule_index = COALESCE((SELECT max_temporary_storage_schedule_index FROM subquery2), -10000000) + " +
-                    "       (SELECT row_num * 10000000 FROM subquery1 WHERE subquery1.schedule_id = s1.schedule_id) " +
-                    "WHERE " +
-                    "   s1.day_id IN :dayIds", nativeQuery = true)
+            "   SELECT " +
+            "       s2.schedule_id, " +
+            "       ROW_NUMBER() OVER (ORDER BY d.trip_date ASC, s2.schedule_index ASC) AS row_num " +
+            "   FROM " +
+            "       schedules s2 " +
+            "       JOIN days d ON s2.day_id = d.day_id " +
+            "   WHERE " +
+            "       s2.day_id IN :dayIds" +
+            "), " +
+            "subquery2 AS (" +
+            "   SELECT " +
+            "       MAX(s3.schedule_index) AS max_temporary_storage_schedule_index " + // 임시보관함의 최대 인덱스값 구하기
+            "   FROM " +
+            "       schedules s3 " +
+            "   WHERE " +
+            "       s3.trip_id = :tripId AND s3.day_id IS NULL" +
+            ") " +
+            "UPDATE " +
+            "   schedules s1 " +
+            "SET " + // 임시보관함의 제일 뒤로 옮기기
+            "   s1.day_id = NULL, " +
+            "   s1.schedule_index = COALESCE((SELECT max_temporary_storage_schedule_index FROM subquery2), -10000000) + " +
+            "       (SELECT row_num * 10000000 FROM subquery1 WHERE subquery1.schedule_id = s1.schedule_id) " +
+            "WHERE " +
+            "   s1.day_id IN :dayIds", nativeQuery = true)
     int moveSchedulesToTemporaryStorage(@Param("tripId") Long tripId, @Param("dayIds") List<Long> dayIds);
 
     @Query("""
