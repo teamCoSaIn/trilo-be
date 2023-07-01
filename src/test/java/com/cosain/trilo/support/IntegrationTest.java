cc6076aa32311447809c2b4f2315bf268cac6273
pia2011 <kks01650165@gmail.com>
2023-07-02 00:36:45 +0900

    [TRL-380] test: 회원 탈퇴 통합 테스트

--- src/test/java/com/cosain/trilo/support/IntegrationTest.java
+++ src/test/java/com/cosain/trilo/support/IntegrationTest.java
@@ -7,12 +7,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class IntegrationTest {
 
     @Autowired
