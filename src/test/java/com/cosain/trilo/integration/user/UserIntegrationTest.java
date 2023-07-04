df9abc8ec72c7f3f8cc8f42d492d9ef596414c8e
ttasjwi <ttasjwi920@gmail.com>
2023-07-04 16:29:42 +0900

    [TRL-386] test: IntegrationTest - 편의 메서드 추가

--- src/test/java/com/cosain/trilo/integration/user/UserIntegrationTest.java
+++ src/test/java/com/cosain/trilo/integration/user/UserIntegrationTest.java
@@ -19,7 +19,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DisplayName("사용자 관련 기능 통합 테스트")
 public class UserIntegrationTest extends IntegrationTest {
     private final String BASE_URL = "/api/users";
-    private final String TOKEN_TYPE = "Bearer ";
 
     @Autowired
     UserRepository userRepository;
@@ -30,13 +29,12 @@ public class UserIntegrationTest extends IntegrationTest {
         void 회원_프로필_조회_성공() throws Exception{
             // given
             User user = setupMockKakaoUser();
-            String accessToken = createAccessToken(user);
 
             log.info("user = {}", user);
 
             // when & then
             mockMvc.perform(RestDocumentationRequestBuilders.get(BASE_URL + "/{userId}/profile", user.getId())
-                            .header(HttpHeaders.AUTHORIZATION, TOKEN_TYPE + accessToken))
+                            .header(HttpHeaders.AUTHORIZATION, authorizationHeader(user)))
                     .andExpect(status().isOk())
                     .andExpect(jsonPath("$.id").value(user.getId()))
                     .andExpect(jsonPath("$.name").value(user.getName()))
@@ -50,14 +48,13 @@ public class UserIntegrationTest extends IntegrationTest {
             // given
             User requestUser = setupMockGoogleUser();
             User targetUser = setupMockKakaoUser();
-            String accessToken = createAccessToken(requestUser);
 
             log.info("requestUser = {}", requestUser);
             log.info("targetUser = {}", targetUser);
 
             // when & then
             mockMvc.perform(RestDocumentationRequestBuilders.get(BASE_URL + "/{userId}/profile", targetUser.getId())
-                            .header(HttpHeaders.AUTHORIZATION, TOKEN_TYPE + accessToken))
+                            .header(HttpHeaders.AUTHORIZATION, authorizationHeader(requestUser)))
                     .andExpect(status().isForbidden())
                     .andExpect(jsonPath("$.errorCode").value("user-0002"))
                     .andExpect(jsonPath("$.errorMessage").exists())
@@ -71,13 +68,11 @@ public class UserIntegrationTest extends IntegrationTest {
         void 회원_탈퇴_성공() throws Exception{
             // given
             User user = setupMockKakaoUser();
-            String accessToken = createAccessToken(user);
-
             log.info("User = {}", user);
 
             // when & then
             mockMvc.perform(RestDocumentationRequestBuilders.delete(BASE_URL + "/{userId}", user.getId())
-                    .header(HttpHeaders.AUTHORIZATION, TOKEN_TYPE + accessToken))
+                    .header(HttpHeaders.AUTHORIZATION, authorizationHeader(user)))
                     .andExpect(status().isNoContent());
 
             User findUser = userRepository.findById(user.getId()).orElse(null);
@@ -89,14 +84,12 @@ public class UserIntegrationTest extends IntegrationTest {
             // given
             User requestUser = setupMockKakaoUser();
             User targetUser = setupMockGoogleUser();
-            String accessToken = createAccessToken(requestUser);
-
             log.info("requestUser = {}", requestUser);
             log.info("targetUser = {}", targetUser);
 
             // when & then
             mockMvc.perform(RestDocumentationRequestBuilders.delete(BASE_URL + "/{userId}", targetUser.getId())
-                            .header(HttpHeaders.AUTHORIZATION, TOKEN_TYPE + accessToken))
+                            .header(HttpHeaders.AUTHORIZATION, authorizationHeader(requestUser)))
                     .andExpect(status().isForbidden())
                     .andExpect(jsonPath("$.errorCode").value("user-0004"))
                     .andExpect(jsonPath("$.errorMessage").exists())
