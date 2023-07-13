99966379d9a83bdd9e3a36046c800866c3f389bb
pia2011 <kks01650165@gmail.com>
2023-07-13 11:13:43 +0900

    [TRL-423] feat: 회원 정보 수정 API Endpoint 구현

--- src/test/java/com/cosain/trilo/unit/user/presentation/UserRestControllerTest.java
+++ src/test/java/com/cosain/trilo/unit/user/presentation/UserRestControllerTest.java
@@ -5,11 +5,15 @@ import com.cosain.trilo.support.RestControllerTest;
 import com.cosain.trilo.user.application.UserService;
 import com.cosain.trilo.user.presentation.UserRestController;
 import com.cosain.trilo.user.presentation.dto.UserProfileResponse;
+import com.cosain.trilo.user.presentation.dto.UserUpdateRequest;
 import org.apache.http.HttpHeaders;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -18,7 +22,10 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 
 import static com.cosain.trilo.fixture.UserFixture.KAKAO_MEMBER;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(UserRestController.class)
@@ -117,11 +124,63 @@ public class UserRestControllerTest extends RestControllerTest {
         }
 
         @Test
-        void 미인증된_사용자_요청_204() throws Exception{
+        void 미인증된_사용자_요청_401() throws Exception{
             // when & then
             mockMvc.perform(RestDocumentationRequestBuilders.get(BASE_URL + "/{userId}/my-page", userId)
                             .header(HttpHeaders.AUTHORIZATION, ACCESS_TOKEN))
                     .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+        }
+    }
+
+    @Nested
+    class 회원_정보_수정{
+        private Long userId = 1L;
+        @Test
+        void 인증된_사용자_요청_200() throws Exception{
+            // given
+            mockingForLoginUserAnnotation();
+            UserUpdateRequest userUpdateRequest = new UserUpdateRequest("nickName");
+
+            // when & then
+            mockMvc.perform(RestDocumentationRequestBuilders.patch(BASE_URL + "/{userId}", userId)
+                            .header(HttpHeaders.AUTHORIZATION, ACCESS_TOKEN)
+                            .content(createJson(userUpdateRequest))
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(MockMvcResultMatchers.status().isOk());
+
+            verify(userService).update(eq(userId), eq(userId), any(UserUpdateRequest.class));
+        }
+
+        @ValueSource(strings = {"", " ", "    ","012345678901234567890"})
+        @ParameterizedTest
+        void 변경하려는_닉네임이_유효하지_않은_요청_400(String nickName) throws Exception{
+            // given
+            mockingForLoginUserAnnotation();
+            UserUpdateRequest userUpdateRequest = new UserUpdateRequest(nickName);
+
+            // when & then
+            mockMvc.perform(RestDocumentationRequestBuilders.patch(BASE_URL + "/{userId}", userId)
+                            .header(HttpHeaders.AUTHORIZATION, ACCESS_TOKEN)
+                            .content(createJson(userUpdateRequest))
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andExpect(jsonPath("$.errorCode").value("request-0001"))
+                    .andExpect(jsonPath("$.errorMessage").exists())
+                    .andExpect(jsonPath("$.errorDetail").exists());
+        }
+
+        @Test
+        void 미인증된_사용자_요청_401() throws Exception{
+            // given
+            UserUpdateRequest userUpdateRequest = new UserUpdateRequest("nickName");
+
+            // when & then
+            mockMvc.perform(RestDocumentationRequestBuilders.patch(BASE_URL + "/{userId}", userId)
+                            .header(HttpHeaders.AUTHORIZATION, ACCESS_TOKEN)
+                            .content(createJson(userUpdateRequest))
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+
         }
     }
 
