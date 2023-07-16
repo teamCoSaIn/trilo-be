57271b0220c4efca8957c15d1f8f8a027cd89035
pia2011 <kks01650165@gmail.com>
2023-07-16 13:19:39 +0900

    [TRL-430] refactor: Spring Security 의존성 및 관련 코드 제거

--- src/test/java/com/cosain/trilo/config/SecurityTestConfig.java
+++
@@ -1,9 +0,0 @@
-package com.cosain.trilo.config;
-
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-
-@TestConfiguration
-@ComponentScan(basePackages = "com.cosain.trilo.config.security")
-public class SecurityTestConfig {
-}
