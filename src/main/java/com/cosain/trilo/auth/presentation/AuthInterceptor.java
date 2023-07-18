7d2854685f5fc90fdc19faeea92b68a10b0f91af
pia2011 <kks01650165@gmail.com>
2023-07-18 10:54:45 +0900

    [TRL-430] refactor: apache 의존성 제거 -> springframework 의존성으로 대체

--- src/main/java/com/cosain/trilo/auth/presentation/AuthInterceptor.java
+++ src/main/java/com/cosain/trilo/auth/presentation/AuthInterceptor.java
@@ -7,7 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.HttpHeaders;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
