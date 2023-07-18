28a8ecbfc2a9ede54a0647205b7d129083cad059
pia2011 <kks01650165@gmail.com>
2023-07-18 11:23:51 +0900

    [TRL-430] refactor: annotation 기반 Equals & HashCode 정의

--- src/main/java/com/cosain/trilo/auth/application/token/UserPayload.java
+++ src/main/java/com/cosain/trilo/auth/application/token/UserPayload.java
@@ -1,25 +1,17 @@
 package com.cosain.trilo.auth.application.token;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.util.Objects;
 
 @Getter
+@EqualsAndHashCode(of = {"id"})
 public class UserPayload {
 
     private final Long id;
 
     public UserPayload(Long id){
         this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
     }
 }
