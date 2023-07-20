fae8e9a14d0d1e51a234d6e5d6e7ddc2185edfcf
pia2011 <kks01650165@gmail.com>
2023-07-20 15:19:56 +0900

    [TRL-430] refactor: 회원 탈퇴 기능 이벤트 처리 방식 변경

--- src/main/java/com/cosain/trilo/trip/application/event/TripEventListener.java
+++ src/main/java/com/cosain/trilo/trip/application/event/TripEventListener.java
@@ -3,9 +3,8 @@ package com.cosain.trilo.trip.application.event;
 import com.cosain.trilo.trip.application.trip.service.trip_all_delete.TripAllDeleteService;
 import com.cosain.trilo.user.application.event.UserDeleteEvent;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
@@ -13,8 +12,7 @@ public class TripEventListener {
 
     private final TripAllDeleteService tripAllDeleteService;
 
-    @Async
-    @TransactionalEventListener
+    @EventListener
     public void handle(UserDeleteEvent event){
         Long tripperId = event.getUserId();
         tripAllDeleteService.deleteAllByTripperId(tripperId);
