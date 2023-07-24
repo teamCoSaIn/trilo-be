2c9a2d3d9b6d2685859c1c15fa21b6e3643bca43
ttasjwi <ttasjwi920@gmail.com>
2023-07-24 15:39:49 +0900

    [TRL-451] docs: 여행생성 - 애플리케이션 계층 주석 및 JavaDoc 작성

--- src/main/java/com/cosain/trilo/trip/application/trip/service/trip_create/TripCreateCommand.java
+++ src/main/java/com/cosain/trilo/trip/application/trip/service/trip_create/TripCreateCommand.java
@@ -9,18 +9,39 @@ import lombok.Getter;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 여행 생성에 필요한 명령(command, 비즈니스 입력 모델)입니다.
+ * @see TripCreateService
+ */
 @Getter
 @EqualsAndHashCode(of = {"tripperId", "tripTitle"})
 public class TripCreateCommand {
 
+    /**
+     * 여행 생성을 시도하는 여행자(사용자) 식별자
+     */
     private final long tripperId;
+
+    /**
+     * 생성하고자 하는 여행의 제목
+     * @see TripTitle
+     */
     private final TripTitle tripTitle;
 
-    public static TripCreateCommand of(Long tripperId, String rawTitle) {
-        List<CustomException> exceptions = new ArrayList<>();
+    /**
+     * 여행 생성 명령(비즈니스 입력 모델)을 생성합니다.
+     * @param tripperId : 여행 생성을 시도하는 여행자(사용자) 식별자)
+     * @param rawTitle : 생성하고자 하는 여행의 제목
+     * @return 여행생성명령
+     * @throws CustomValidationException : 명령 생성과정에서 발생한 예외들을 묶은 예외
+     */
+    public static TripCreateCommand of(Long tripperId, String rawTitle) throws CustomValidationException {
+        List<CustomException> exceptions = new ArrayList<>(); // 발생 예외를 수집할 예외 수집기
 
-        TripTitle tripTitle = makeTripTitle(rawTitle, exceptions);
+        TripTitle tripTitle = makeTripTitleAndValidate(rawTitle, exceptions); // 여행 제목 생성 및 비즈니스 입력 검증
+
         if (!exceptions.isEmpty()) {
+            // 입력 검증 과정에서 예외가 하나라도 발생할 경우 이들을 모아서, 검증 예외를 발생시킴.
             throw new CustomValidationException(exceptions);
         }
         return new TripCreateCommand(tripperId, tripTitle);
@@ -31,10 +52,19 @@ public class TripCreateCommand {
         this.tripTitle = tripTitle;
     }
 
-    private static TripTitle makeTripTitle(String rawTitle, List<CustomException> exceptions) {
+    /**
+     * <p>여행제목({@link TripTitle})을 생성하고, 이 과정에서 여행 제목에 대한 입력 검증을 수행합니다.</p>
+     * <p>여행제목 생성 과정에서 예외가 발생하면 예외 수집기에 비즈니스 예외를 수집합니다.</p>
+     * @param rawTitle : 여행의 제목
+     * @param exceptions : 검증 과정에서 발생한 예외를 수집할 컬렉션
+     * @return 정상적인 입력이면 여행 제목 객체를, 그렇지 않을 경우 null 반환
+     * @see TripTitle
+     */
+    private static TripTitle makeTripTitleAndValidate(String rawTitle, List<CustomException> exceptions) {
         try {
             return TripTitle.of(rawTitle);
         } catch (CustomException e) {
+            // 비즈니스 예외가 발생할 경우 예외 수집
             exceptions.add(e);
         }
         return null;
--- src/main/java/com/cosain/trilo/trip/application/trip/service/trip_create/TripCreateService.java
+++ src/main/java/com/cosain/trilo/trip/application/trip/service/trip_create/TripCreateService.java
@@ -6,20 +6,27 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+ * 여행 생성을 수행하는 애플리케이션 서비스입니다.
+ */
 @Service
 @RequiredArgsConstructor
 public class TripCreateService {
 
+    /**
+     * 생성 여행을 저장할 리포지토리
+     */
     private final TripRepository tripRepository;
 
     /**
-     * Trip을 생성하여 등록하고, 식별자를 반환합니다.
-     * @param createCommand : 여행 생성에 필요한 정보들
-     * @return 생성된 여행의 식별자
+     * Trip(여행)을 생성하여 등록하고, 생성된 여행의 식별자(id)를 반환합니다.
+     * @param command : 여행 생성 명령(비즈니스 입력 모델)
+     * @return 생성된 여행의 식별자(id)
+     * @see TripCreateCommand
      */
     @Transactional
-    public Long createTrip(TripCreateCommand createCommand) {
-        Trip trip = Trip.create(createCommand.getTripTitle(), createCommand.getTripperId());
+    public Long createTrip(TripCreateCommand command) {
+        Trip trip = Trip.create(command.getTripTitle(), command.getTripperId());
         Trip savedTrip = tripRepository.save(trip);
         return savedTrip.getId();
     }
