67f38474cc6fd9e2cb648c1b68422542c68722cc
ttasjwi <ttasjwi920@gmail.com>
2023-07-24 17:06:52 +0900

    [TRL-451] docs: 여행 도메인 엔티티, 값 객체 -> 주석, JavaDoc 작성

--- src/main/java/com/cosain/trilo/trip/domain/vo/TripImage.java
+++ src/main/java/com/cosain/trilo/trip/domain/vo/TripImage.java
@@ -7,25 +7,47 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+/**
+ * 여행이미지 VO(값 객체)입니다.
+ */
 @Getter
 @ToString(of = {"fileName"})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Embeddable
 public class TripImage {
 
-    public static final String DEFAULT_IMAGE_NAME = "trips/trip-default-image.png";
+    /**
+     * 여행이미지의 디폴트 파일명
+     */
+    private static final String DEFAULT_IMAGE_NAME = "trips/trip-default-image.png";
+
+    /**
+     * 여행이미지 디폴트 VO(값 객체)
+     */
     private static final TripImage DEFAULT_IMAGE = new TripImage(DEFAULT_IMAGE_NAME);
 
+    /**
+     * 여행이미지의 파일명
+     */
     @Column(name = "trip_image_file_name")
     private String fileName;
 
+    /**
+     * 여행이미지 VO(값 객체)를 생성합니다.
+     * @param fileName : 이미지 파일명
+     * @return : 여행 이미지 VO(값 객체)
+     */
     public static TripImage of(String fileName) {
-        if (fileName.equals(DEFAULT_IMAGE_NAME)) {
-            return DEFAULT_IMAGE;
-        }
-        return new TripImage(fileName);
+        return fileName.equals(DEFAULT_IMAGE_NAME)
+                ? DEFAULT_IMAGE
+                : new TripImage(fileName);
     }
 
+    /**
+     * 여행이미지 디폴트 VO(값 객체)를 반환합니다.
+     * @return 여행 이미지 디폴트 VO(값 객체)
+     * @see TripImage#DEFAULT_IMAGE
+     */
     public static TripImage defaultImage() {
         return DEFAULT_IMAGE;
     }
--- src/main/java/com/cosain/trilo/trip/domain/vo/TripTitle.java
+++ src/main/java/com/cosain/trilo/trip/domain/vo/TripTitle.java
@@ -5,6 +5,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.*;
 
+/**
+ * 여행제목 VO(값 객체)입니다.
+ */
 @Getter
 @ToString(of = {"value"})
 @EqualsAndHashCode(of = {"value"})
@@ -12,23 +15,49 @@ import lombok.*;
 @Embeddable
 public class TripTitle {
 
+    /**
+     * 여행 제목의 최소 길이 제한입니다.
+     */
     private static final int MIN_LENGTH = 1;
+
+    /**
+     * 여행 제목의 최대 길이 제한입니다.
+     */
     private static final int MAX_LENGTH = 20;
 
+    /**
+     * 여행제목의 원시 문자열 값입니다.
+     */
     @Column(name = "trip_title")
     private String value;
 
-    public static TripTitle of(String value) {
+    /**
+     * 여행제목 VO(값 객체)를 생성합니다.
+     * @param value : 여행 제목 문자열
+     * @return 여행 제목 VO(값 객체)
+     * @throws InvalidTripTitleException : 여행 제목이 유효하지 않을 때
+     */
+    public static TripTitle of(String value) throws InvalidTripTitleException {
         if (isNullOrBlank(value) || hasInvalidLength(value)) {
             throw new InvalidTripTitleException("null 또는 공백이거나, 길이 조건에 맞지 않는 여행 제목");
         }
         return new TripTitle(value);
     }
 
+    /**
+     * 여행 제목이 null 또는 공백으로만 구성됐는 지 여부를 반환합니다.
+     * @param value : 여행 제목 문자열
+     * @return 여행 제목이 null 이거나 공백으로만 구성되면 true, 아니면 false
+     */
     private static boolean isNullOrBlank(String value) {
         return value == null || value.isBlank();
     }
 
+    /**
+     * 여행 제목이 유효하지 않은 길이를 가졌는 지 여부를 반환합니다.
+     * @param value : 여행 제목
+     * @return 여행 제목 길이가 유효하지 않으면 true, 유효하면 false
+     */
     private static boolean hasInvalidLength(String value) {
         return value.length() < MIN_LENGTH || value.length() > MAX_LENGTH;
     }
