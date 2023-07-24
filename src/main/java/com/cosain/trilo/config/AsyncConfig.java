100ee753c7e5def901ea43bb49949c441ab543fe
pia2011 <kks01650165@gmail.com>
2023-07-24 22:39:02 +0900

    [TRL-444] feat: 스레드 풀 method-level 설정 추가 및 회원 삭제 이벤트 처리 방식 비동기로 수정

--- src/main/java/com/cosain/trilo/config/AsyncConfig.java
+++ src/main/java/com/cosain/trilo/config/AsyncConfig.java
@@ -1,9 +1,28 @@
 package com.cosain.trilo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
 public class AsyncConfig {
+    private static int CORE_POOL_SIZE = 15; // 동시에 실행시킬 쓰레드의 개수 ( default : 1 )
+    private static int MAX_POOL_SIZE = 25; // 쓰레드 풀의 최대 사이즈를 지정 ( default : Integer.MAX_VALUE )
+    private static int QUEUE_CAPACITY = 10; // 큐의 사이즈 ( default : Integer.MAX_VALUE )
+    private static String THREAD_NAME_PREFIX = "async-task"; // Thread name prefix
+
+    @Bean(name = "threadPoolTaskExecutor")
+    public Executor asyncTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(CORE_POOL_SIZE);
+        executor.setMaxPoolSize(MAX_POOL_SIZE);
+        executor.setQueueCapacity(QUEUE_CAPACITY);
+        executor.setThreadNamePrefix(THREAD_NAME_PREFIX);
+        executor.initialize();
+        return executor;
+    }
 }
--- src/main/java/com/cosain/trilo/trip/application/event/TripEventListener.java
+++ src/main/java/com/cosain/trilo/trip/application/event/TripEventListener.java
@@ -3,8 +3,9 @@ package com.cosain.trilo.trip.application.event;
 import com.cosain.trilo.trip.application.trip.service.trip_all_delete.TripAllDeleteService;
 import com.cosain.trilo.user.application.event.UserDeleteEvent;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
@@ -12,7 +13,8 @@ public class TripEventListener {
 
     private final TripAllDeleteService tripAllDeleteService;
 
-    @EventListener
+    @Async("threadPoolTaskExecutor")
+    @TransactionalEventListener
     public void handle(UserDeleteEvent event){
         Long tripperId = event.getUserId();
         tripAllDeleteService.deleteAllByTripperId(tripperId);
