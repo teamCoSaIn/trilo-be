36ebbf6d42166c4b126a4eee1aa25203e274d6a6
pia2011 <kks01650165@gmail.com>
2023-07-28 19:09:14 +0900

    [TRL-455] test: 상위 클래스에 여행 생성 메서드 오버로딩

--- src/test/java/com/cosain/trilo/integration/trip/TripConditionSearchIntegrationTest.java
+++ src/test/java/com/cosain/trilo/integration/trip/TripConditionSearchIntegrationTest.java
@@ -1,15 +1,12 @@
 package com.cosain.trilo.integration.trip;
 
-import com.cosain.trilo.fixture.TripFixture;
 import com.cosain.trilo.support.IntegrationTest;
 import com.cosain.trilo.trip.domain.entity.Trip;
-import com.cosain.trilo.trip.domain.repository.TripRepository;
 import com.cosain.trilo.trip.presentation.trip.dto.request.TripSearchRequest;
 import com.cosain.trilo.user.domain.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.ResultActions;
@@ -23,9 +20,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DisplayName("[통합] 여행 목록 조회 API 테스트")
 public class TripConditionSearchIntegrationTest extends IntegrationTest {
 
-    @Autowired
-    private TripRepository tripRepository;
-
     private static final String BASE_URL = "/api/trips";
 
     @Nested
@@ -35,10 +29,12 @@ public class TripConditionSearchIntegrationTest extends IntegrationTest {
 
             // given
             User user = setupMockKakaoUser();
-            setupMockTrip("제천 가자", user.getId());
-            setupMockTrip("강원도 여행", user.getId());
-            setupMockTrip("제주도 여행", user.getId());
-            setupMockTrip("제주 1박 2일 여행", user.getId());
+            LocalDate startDate = LocalDate.of(2023, 3, 10);
+            LocalDate endDate = LocalDate.of(2023, 3, 14);
+            setupDecidedTrip(user.getId(),startDate,endDate,"제천 가자");
+            setupDecidedTrip(user.getId(),startDate,endDate,"강원도 여행");
+            setupDecidedTrip(user.getId(),startDate,endDate,"제주도 여행");
+            setupDecidedTrip(user.getId(),startDate,endDate,"제주 1박 2일 여행");
 
             int size = 5;
             String query = "제주";
@@ -67,10 +63,12 @@ public class TripConditionSearchIntegrationTest extends IntegrationTest {
 
             // given
             User user = setupMockKakaoUser();
-            setupMockTrip("제천 가자", user.getId());
-            setupMockTrip("강원도 여행", user.getId());
-            setupMockTrip("제주도 여행", user.getId());
-            Trip trip = setupMockTrip("제주 1박 2일 여행", user.getId());
+            LocalDate startDate = LocalDate.of(2023, 3, 10);
+            LocalDate endDate = LocalDate.of(2023, 3, 14);
+            setupDecidedTrip(user.getId(),startDate,endDate,"제천 가자");
+            setupDecidedTrip(user.getId(),startDate,endDate,"강원도 여행");
+            setupDecidedTrip(user.getId(),startDate,endDate,"제주도 여행");
+            Trip trip = setupDecidedTrip(user.getId(), startDate, endDate, "제주 1박 2일 여행");
 
             int size = 5;
             String query = "제주";
@@ -97,9 +95,4 @@ public class TripConditionSearchIntegrationTest extends IntegrationTest {
 
     }
 
-    private Trip setupMockTrip(String rawTitle, Long tripperId) {
-        Trip trip = TripFixture.decided_nullId_Title(tripperId, rawTitle, LocalDate.of(2023, 5, 15), LocalDate.of(2023, 5, 20));
-        tripRepository.save(trip);
-        return trip;
-    }
 }
