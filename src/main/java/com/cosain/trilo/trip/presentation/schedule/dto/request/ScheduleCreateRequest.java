869e2b7e24a8a47f5b819be0a007f26a73907277
ttasjwi <ttasjwi920@gmail.com>
2023-08-02 16:27:36 +0900

    [TRL-461] docs: 일정 생성 - 표현계층 JavaDoc, 주석 작성

--- src/main/java/com/cosain/trilo/trip/presentation/schedule/dto/request/ScheduleCreateRequest.java
+++ src/main/java/com/cosain/trilo/trip/presentation/schedule/dto/request/ScheduleCreateRequest.java
@@ -6,19 +6,42 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 일정 생성을 위한 요청 정보를 이 객체에 바인딩합니다.
+ */
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ScheduleCreateRequest {
 
+    /**
+     * Day의 식별자(null 일 경우 임시보관함)
+     */
     private Long dayId;
 
+    /**
+     * 여행의 식별자
+     */
     @NotNull(message = "schedule-0007")
     private Long tripId;
 
+    /**
+     * 일정의 제목
+     */
     private String title;
+
+    /**
+     * 장소 식별자
+     */
     private String placeId;
+
+    /**
+     * 장소명
+     */
     private String placeName;
 
+    /**
+     * 좌표({@link CoordinateDto )
+     */
     @NotNull(message = "place-0002")
     private CoordinateDto coordinate;
 
@@ -35,11 +58,21 @@ public class ScheduleCreateRequest {
         this.coordinate = coordinate;
     }
 
+    /**
+     * 일정 생성 요청으로 전달된 좌표를 바인딩하는 객체입니다.
+     */
     @Getter
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
     public static class CoordinateDto {
 
+        /**
+         * 위도
+         */
         private Double latitude;
+
+        /**
+         * 경도
+         */
         private Double longitude;
 
         public CoordinateDto(double latitude, double longitude) {
